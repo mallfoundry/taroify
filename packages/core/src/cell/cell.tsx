921f05dbc25de544bdd58c38f895c5db8d235315
TangZhi <tgioer@163.com>
2021-03-22 15:15:07 +0800

    完善 Cell 组件。

--- packages/core/src/cell/cell.tsx
+++ packages/core/src/cell/cell.tsx
@@ -1,8 +1,39 @@
-interface CellProps {
+import { View } from "@tarojs/components"
+import * as React from "react"
+import { ReactNode } from "react"
+import classNames from "classnames"
+import { prefixClassname } from "../styles"
+import ArrowForwardIosOutlined from "@vant-taro/icons/ArrowForwardIosOutlined"
 
+interface CellProps {
+  className?: string
+  label?: ReactNode
+  arrow?: boolean
+  endIcon?: ReactNode
+  clickable?: boolean
+  children?: ReactNode
 }
 
-
 export default function Cell(props: CellProps) {
-
+  const { className, label, clickable = false, arrow = false, children } = props
+  return (
+    <View className={classNames(
+      prefixClassname("cell"),
+      {
+        [prefixClassname(`cell-clickable`)]: clickable,
+        // [prefixClassname(`col-offset-${offset}`)]: offset !== undefined,
+      },
+      className,
+    )}>
+      <View className={prefixClassname("cell-label")}>{label}</View>
+      <View className={prefixClassname("cell-value")}>{children}</View>
+      {
+        arrow && (
+          <View className={prefixClassname("cell-arrow")}>
+            <ArrowForwardIosOutlined color="inherit" size="inherit" />
+          </View>
+        )
+      }
+    </View>
+  )
 }
