da8f79b4e1d159399510695f82b99c44352160b2
TangZhi <tgioer@163.com>
2021-03-22 15:15:36 +0800

    优化 Popup 组件。

--- packages/core/src/popup/popup.tsx
+++ packages/core/src/popup/popup.tsx
@@ -1,60 +1,74 @@
 import * as React from "react"
 import { ReactNode } from "react"
-import * as classNames from "classnames"
+import classNames from "classnames"
 import { View } from "@tarojs/components"
 import { prefixClassname } from "../styles"
-import Overlay from "../overlay"
+import Backdrop from "../backdrop"
 
-interface PopupHeaderProps {
-  closeable?: boolean;
-  children?: ReactNode
-  onClose?: () => void
+// interface PopupHeaderProps {
+//   closeable?: boolean;
+//   children?: ReactNode
+//   onClose?: () => void
+// }
+//
+// function PopupHeader({ closeable, children, onClose }: PopupHeaderProps) {
+//   return (
+//     <View className={prefixClassname("popup-header")}>
+//       {children}
+//       {/*{closeable && <AtIcon className={prefixClassName("popup-close")} value="close" onClick={onClose} />}*/}
+//     </View>
+//   )
+// }
+
+// interface PopupContainerProps {
+//   closeable?: boolean;
+//   children?: ReactNode;
+//   onClose?: () => void
+// }
+//
+// function PopupContainer({ closeable, children, onClose }: PopupContainerProps) {
+//   return (
+//     <View className={prefixClassname("popup-container")}>
+//       {/*<PopupHeader closeable={closeable} onClose={onClose} />*/}
+//       {children}
+//     </View>
+//   )
+// }
+
+export enum PopupAnchor {
+  Top = "top",
+  Right = "right",
+  Bottom = "bottom",
+  Left = "left",
 }
 
-function PopupHeader({ closeable, children, onClose }: PopupHeaderProps) {
-  return (
-    <View className={prefixClassname("popup-header")}>
-      {children}
-      {/*{closeable && <AtIcon className={prefixClassName("popup-close")} value="close" onClick={onClose} />}*/}
-    </View>
-  )
-}
-
-interface PopupContainerProps {
-  closeable?: boolean;
-  children?: ReactNode;
-  onClose?: () => void
-}
-
-function PopupContainer({ closeable, children, onClose }: PopupContainerProps) {
-  return (
-    <View className={prefixClassname("popup-container")}>
-      <PopupHeader closeable={closeable} onClose={onClose} />
-      {children}
-    </View>
-  )
-}
+type PopupAnchorString = "top" | "right" | "bottom" | "left"
 
 interface PopupProps {
   open?: boolean
+  anchor?: PopupAnchor | PopupAnchorString
   closeable?: boolean
   children?: ReactNode
   onClose?: () => void
 }
 
 export default function Popup(props: PopupProps) {
-  const { open, closeable, children, onClose } = props
-
+  const { open, anchor = PopupAnchor.Bottom, closeable, children, onClose } = props
+  console.log(closeable)
   return (
-    <View className={classNames(prefixClassname("popup"),
-      {
-        [prefixClassname("popup-open")]: open,
-      })}
-    >
-      <Overlay open={open} closable onClose={onClose} />
-      <PopupContainer closeable={closeable} onClose={onClose}>
+    <>
+      <Backdrop open={open} closable onClose={onClose} />
+      <View
+        className={classNames(prefixClassname("popup"),
+          {
+            [prefixClassname("popup-open")]: open,
+            [prefixClassname("popup-anchor-top")]: anchor === PopupAnchor.Top,
+            [prefixClassname("popup-anchor-right")]: anchor === PopupAnchor.Right,
+            [prefixClassname("popup-anchor-bottom")]: anchor === PopupAnchor.Bottom,
+            [prefixClassname("popup-anchor-left")]: anchor === PopupAnchor.Left,
+          })}>
         {children}
-      </PopupContainer>
-    </View>
+      </View>
+    </>
   )
 }
