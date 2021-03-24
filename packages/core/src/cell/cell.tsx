0e3b85f97196194cc45cd66959678c202462f07e
TangZhi <tgioer@163.com>
2021-03-24 13:07:13 +0800

    添加 border 支持。

--- packages/core/src/cell/cell.tsx
+++ packages/core/src/cell/cell.tsx
@@ -3,34 +3,37 @@ import * as React from "react"
 import { ReactNode } from "react"
 import classNames from "classnames"
 import { prefixClassname } from "../styles"
-import ArrowForwardIosOutlined from "@vant-taro/icons/ArrowForwardIosOutlined"
+import { ITouchEvent } from "@tarojs/components/types/common"
 
 interface CellProps {
   className?: string
   label?: ReactNode
-  arrow?: boolean
   endIcon?: ReactNode
+  bordered?: boolean
   clickable?: boolean
   children?: ReactNode
+  onClick?: (event: ITouchEvent) => void
 }
 
 export default function Cell(props: CellProps) {
-  const { className, label, clickable = false, arrow = false, children } = props
+  const { className, label, clickable = false, bordered = true, endIcon, children, onClick } = props
   return (
-    <View className={classNames(
-      prefixClassname("cell"),
-      {
-        [prefixClassname(`cell-clickable`)]: clickable,
-        // [prefixClassname(`col-offset-${offset}`)]: offset !== undefined,
-      },
-      className,
-    )}>
+    <View
+      className={classNames(
+        prefixClassname("cell"),
+        {
+          [prefixClassname(`cell-clickable`)]: clickable,
+          [prefixClassname(`cell-borderless`)]: !bordered,
+        },
+        className,
+      )}
+      onClick={onClick}>
       <View className={prefixClassname("cell-label")}>{label}</View>
       <View className={prefixClassname("cell-value")}>{children}</View>
       {
-        arrow && (
-          <View className={prefixClassname("cell-arrow")}>
-            <ArrowForwardIosOutlined color="inherit" size="inherit" />
+        endIcon && (
+          <View className={prefixClassname("cell-end-icon")}>
+            {endIcon}
           </View>
         )
       }
