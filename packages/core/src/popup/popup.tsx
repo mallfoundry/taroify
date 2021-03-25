e09650d182c20818402a483a0a56d6973499c4a1
TangZhi <tgioer@163.com>
2021-03-25 12:05:17 +0800

    添加弹出动画。

--- packages/core/src/popup/popup.tsx
+++ packages/core/src/popup/popup.tsx
@@ -1,39 +1,11 @@
 import * as React from "react"
-import { ReactNode } from "react"
+import { CSSProperties, ReactNode } from "react"
 import classNames from "classnames"
 import { View } from "@tarojs/components"
 import { prefixClassname } from "../styles"
 import Backdrop from "../backdrop"
-
-// interface PopupHeaderProps {
-//   closeable?: boolean;
-//   children?: ReactNode
-//   onClose?: () => void
-// }
-//
-// function PopupHeader({ closeable, children, onClose }: PopupHeaderProps) {
-//   return (
-//     <View className={prefixClassname("popup-header")}>
-//       {children}
-//       {/*{closeable && <AtIcon className={prefixClassName("popup-close")} value="close" onClick={onClose} />}*/}
-//     </View>
-//   )
-// }
-
-// interface PopupContainerProps {
-//   closeable?: boolean;
-//   children?: ReactNode;
-//   onClose?: () => void
-// }
-//
-// function PopupContainer({ closeable, children, onClose }: PopupContainerProps) {
-//   return (
-//     <View className={prefixClassname("popup-container")}>
-//       {/*<PopupHeader closeable={closeable} onClose={onClose} />*/}
-//       {children}
-//     </View>
-//   )
-// }
+import * as _ from "lodash"
+import Transition, { TransitionName } from "../transition"
 
 export enum PopupAnchor {
   Top = "top",
@@ -44,31 +16,83 @@ export enum PopupAnchor {
 
 type PopupAnchorString = "top" | "right" | "bottom" | "left"
 
+function getTransactionName(anchor?: PopupAnchor | PopupAnchorString) {
+
+  if (anchor === PopupAnchor.Top) {
+    return TransitionName.SlideDown
+  }
+
+  if (anchor === PopupAnchor.Bottom) {
+    return TransitionName.SlideUp
+  }
+
+  if (anchor === PopupAnchor.Right) {
+    return TransitionName.SlideRight
+  }
+
+  if (anchor === PopupAnchor.Left) {
+    return TransitionName.SlideLeft
+  }
+
+  return TransitionName.Fade
+}
+
 interface PopupProps {
+  className?: string
+  style?: CSSProperties
   open?: boolean
   anchor?: PopupAnchor | PopupAnchorString
+  rounded?: boolean
+  duration?: number
   closeable?: boolean
+  backdrop?: boolean
+  backdropCloseable?: boolean
   children?: ReactNode
   onClose?: () => void
 }
 
 export default function Popup(props: PopupProps) {
-  const { open, anchor = PopupAnchor.Bottom, closeable, children, onClose } = props
-  console.log(closeable)
+  const {
+    className,
+    style,
+    open,
+    anchor,
+    rounded = false,
+    duration,
+    // closeable,
+    backdrop = true,
+    backdropCloseable = true,
+    children,
+    onClose,
+  } = props
+
+  const transactionName = getTransactionName(anchor)
   return (
     <>
-      <Backdrop open={open} closable onClose={onClose} />
-      <View
-        className={classNames(prefixClassname("popup"),
-          {
-            [prefixClassname("popup-open")]: open,
-            [prefixClassname("popup-anchor-top")]: anchor === PopupAnchor.Top,
-            [prefixClassname("popup-anchor-right")]: anchor === PopupAnchor.Right,
-            [prefixClassname("popup-anchor-bottom")]: anchor === PopupAnchor.Bottom,
-            [prefixClassname("popup-anchor-left")]: anchor === PopupAnchor.Left,
-          })}>
-        {children}
-      </View>
+      {
+        backdrop && <Backdrop
+          open={open && backdrop}
+          duration={duration}
+          closeable={backdropCloseable}
+          onClose={onClose} />
+      }
+      <Transition in={open} name={transactionName} duration={duration}>
+        <View
+          className={classNames(prefixClassname("popup"),
+            {
+              [prefixClassname("popup-open")]: open,
+              [prefixClassname("popup-rounded")]: rounded,
+              [prefixClassname("popup-center")]: _.isUndefined(anchor),
+              [prefixClassname("popup-anchor-top")]: anchor === PopupAnchor.Top,
+              [prefixClassname("popup-anchor-right")]: anchor === PopupAnchor.Right,
+              [prefixClassname("popup-anchor-bottom")]: anchor === PopupAnchor.Bottom,
+              [prefixClassname("popup-anchor-left")]: anchor === PopupAnchor.Left,
+            },
+            className)}
+          style={style}>
+          {children}
+        </View>
+      </Transition>
     </>
   )
 }
