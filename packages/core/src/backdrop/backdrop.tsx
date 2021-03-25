b64cadd4af01a776987292d0976d6f3cbb7d83a5
TangZhi <tgioer@163.com>
2021-03-25 12:01:33 +0800

    添加 duration 属性。

--- packages/core/src/backdrop/backdrop.tsx
+++ packages/core/src/backdrop/backdrop.tsx
@@ -7,12 +7,13 @@ import { prefixClassname } from "../styles"
 interface BackdropProps {
   open?: boolean
   closeable?: boolean
+  duration?: number
   children?: ReactNode
   onClose?: (event: ITouchEvent) => void
 }
 
 export default function Backdrop(props: BackdropProps) {
-  const { open = false, closeable = false, children, onClose } = props
+  const { open = false, closeable = false, duration = 300, children, onClose } = props
 
   function handleClose(event: ITouchEvent) {
     if (closeable && onClose) {
@@ -25,6 +26,9 @@ export default function Backdrop(props: BackdropProps) {
       className={classNames(prefixClassname("backdrop"), {
         [prefixClassname("backdrop-open")]: open,
       })}
+      style={{
+        transitionDuration: `${duration}ms`,
+      }}
       onClick={handleClose}>
       {children}
     </View>
