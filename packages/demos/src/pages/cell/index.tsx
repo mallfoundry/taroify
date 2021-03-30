bffecc1ab00a5a8ef9313de28cfe97ca7c36ab01
TangZhi <tgioer@163.com>
2021-03-30 11:27:32 +0800

    添加 cell bordered={false} 的演示。

--- packages/demos/src/pages/cell/index.tsx
+++ packages/demos/src/pages/cell/index.tsx
@@ -1,4 +1,4 @@
-import { Cell } from "@vant-taro/core"
+import Cell from "@vant-taro/core/cell"
 import "./index.scss"
 import Block from "../../components/block"
 import Page from "../../components/page"
@@ -8,8 +8,9 @@ export default function CellDemo() {
     <Page title="Cell 单元格">
       <Block title="基础用法">
         <Cell label="标题">内容</Cell>
+        <Cell label="标题" clickable bordered={false}>内容</Cell>
         <Cell label="标题" clickable>内容</Cell>
-        <Cell label="标题" clickable arrow>内容</Cell>
+        <Cell label="标题" clickable>内容</Cell>
       </Block>
     </Page>
   )
