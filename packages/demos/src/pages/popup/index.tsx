65304ac8de950b91f90e94d0b7f824d593e0b1c8
TangZhi <tgioer@163.com>
2021-03-30 11:30:43 +0800

    添加多种 Popup 演示方式。

--- packages/demos/src/pages/popup/index.tsx
+++ packages/demos/src/pages/popup/index.tsx
@@ -1,29 +1,86 @@
 import "@vant-taro/core/index.scss"
 import Page from "../../components/page"
-import { useState } from "react"
-import { Button, Popup } from "@vant-taro/core"
+import { CSSProperties, useState } from "react"
+import Popup, { PopupAnchor } from "@vant-taro/core/popup"
 import Block from "../../components/block"
-import { View } from "@tarojs/components"
+import ArrowForwardIosOutlined from "@vant-taro/icons/ArrowForwardIosOutlined"
+import Cell from "@vant-taro/core/cell"
+
+interface OpenOptions {
+  open?: boolean,
+  style?: CSSProperties
+  anchor?: PopupAnchor
+  rounded?: boolean
+}
 
 export default function PopupDemo() {
-  const [open, setOpen] = useState(false)
-  const [opened2, setOpened2] = useState(false)
-  console.log(opened2, setOpened2)
+  const [options, setOptions] = useState<OpenOptions>({})
+
+  function handleOpen({ anchor, rounded }: OpenOptions) {
+    const openOptions: OpenOptions = {
+      open: true,
+      anchor,
+      rounded,
+    }
+    if (anchor === PopupAnchor.Left || anchor === PopupAnchor.Right) {
+      openOptions.style = {
+        height: "20%",
+        width: "20%",
+      }
+    } else if (anchor) {
+      openOptions.style = { height: "30%" }
+
+    } else {
+      openOptions.style = { padding: "30px 50px" }
+    }
+    setOptions({
+      ...options,
+      ...openOptions,
+    })
+  }
 
   return (
     <Page title="Popup">
       <Block title="基础用法">
-        <Button
-          variant="contained" color="primary"
-          onClick={() => setOpen(true)}>显示遮盖层</Button>
-        <Popup
-          open={open}
-          anchor="top"
-          onClose={() => setOpen(false)}>
-          <View>dsafjaklfjdlk</View>
-          <View>dsafjaklfjdlk</View>
-        </Popup>
+        <Cell clickable label="显示遮盖层"
+              endIcon={<ArrowForwardIosOutlined color="inherit" size="inherit" />}
+              onClick={() => handleOpen({ anchor: undefined })}
+        />
       </Block>
+      <Block title="弹出位置">
+        <Cell clickable label="顶部弹出"
+              endIcon={<ArrowForwardIosOutlined color="inherit" size="inherit" />}
+              onClick={() => handleOpen({ anchor: PopupAnchor.Top })}
+        />
+        <Cell clickable label="底部弹出"
+              endIcon={<ArrowForwardIosOutlined color="inherit" size="inherit" />}
+              onClick={() => handleOpen({ anchor: PopupAnchor.Bottom })}
+        />
+        <Cell clickable label="左侧弹出"
+              endIcon={<ArrowForwardIosOutlined color="inherit" size="inherit" />}
+              onClick={() => handleOpen({ anchor: PopupAnchor.Left })}
+        />
+        <Cell clickable label="右侧弹出"
+              endIcon={<ArrowForwardIosOutlined color="inherit" size="inherit" />}
+              onClick={() => handleOpen({ anchor: PopupAnchor.Right })}
+        />
+      </Block>
+      <Block title="圆角弹窗">
+        <Cell clickable label="圆角弹窗"
+              endIcon={<ArrowForwardIosOutlined color="inherit" size="inherit" />}
+              onClick={() => handleOpen({ anchor: PopupAnchor.Bottom, rounded: true })}
+        />
+      </Block>
+      <Popup
+        open={options.open}
+        anchor={options.anchor}
+        rounded={options.rounded}
+        style={options.style}
+        onClose={() => setOptions({
+          ...options,
+          open: false,
+        })}
+      />
     </Page>
   )
 }
