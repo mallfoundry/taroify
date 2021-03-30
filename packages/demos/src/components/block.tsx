77e18e7993bc69e645ea133afacf95822361a982
TangZhi <tgioer@163.com>
2021-03-30 11:23:03 +0800

    删除无用 console。

--- packages/demos/src/components/block.tsx
+++ packages/demos/src/components/block.tsx
@@ -1,18 +1,14 @@
 import { ReactNode } from "react"
 import { View } from "@tarojs/components"
-
 import classes from "./block.module.scss"
 
-interface DemoBlockProps {
+interface BlockProps {
   title?: ReactNode
   children?: ReactNode
 }
 
-export default function Block(props: DemoBlockProps) {
+export default function Block(props: BlockProps) {
   const { title, children } = props
-  const whitespace = 1
-  const wingblank = 1
-  console.log(whitespace, wingblank)
   return (
     <View className={classes.Block}>
       <View className={classes.BlockTitle} children={title} />
