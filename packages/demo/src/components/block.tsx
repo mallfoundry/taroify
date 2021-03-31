59003aa7bc96a5f940cfdd71bdd129f026b491e4
TangZhi <tgioer@163.com>
2021-03-31 17:47:46 +0800

    优化 block 样式。

--- packages/demo/src/components/block.tsx
+++ packages/demo/src/components/block.tsx
@@ -2,16 +2,18 @@ import * as React from "react"
 import { ReactNode } from "react"
 import { View } from "@tarojs/components"
 import classes from "./block.module.scss"
+import classNames from "classnames"
 
 interface BlockProps {
+  className?: string
   title?: ReactNode
   children?: ReactNode
 }
 
 export default function Block(props: BlockProps) {
-  const { title, children } = props
+  const { className, title, children } = props
   return (
-    <View className={classes.Block}>
+    <View className={classNames(classes.Block, className)}>
       <View className={classes.BlockTitle} children={title} />
       <View children={children} />
     </View>
