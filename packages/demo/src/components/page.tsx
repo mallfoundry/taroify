d9f2bfb0c02c38c9eca75de66b2f1b12d709d420
TangZhi <tgioer@163.com>
2021-03-31 17:48:22 +0800

    添加 back 按钮。

--- packages/demo/src/components/page.tsx
+++ packages/demo/src/components/page.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { ReactNode } from "react"
 import { View } from "@tarojs/components"
+import { navigateBack } from "@tarojs/taro"
+import ArrowLeft from "@taroify/icons/ArrowLeft"
 import classNames from "classnames"
 import classes from "./page.module.scss"
 
@@ -12,9 +14,11 @@ interface PageProps {
 
 export default function Page(props: PageProps) {
   const { className, title, children } = props
+
   return (
     <View className={classNames(classes.Page, className)}>
       <View className={classes.Nav}>
+        <ArrowLeft className={classes.NavBack} onClick={() => navigateBack()} />
         <View className={classes.NavTitle}>{title} </View>
       </View>
       <View
