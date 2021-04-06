8299a85d0f4b07e655c9c91b7d122aa553443dae
TangZhi <tgioer@163.com>
2021-04-06 14:57:18 +0800

    添加 framed-router 工具类。

---
+++ packages/demo/src/utils/framed-router.ts
@@ -0,0 +1,41 @@
+interface Message {
+  source: string
+  event: string
+  payload?: any
+}
+
+const SOURCE_NAME = "taroify-simulator"
+
+const NAVIGATE_BACK_MESSAGE: Message = {
+  source: SOURCE_NAME,
+  event: "navigateBack",
+}
+
+function sendMessage(message: Message) {
+  if (framedWrapper()) {
+    parent.postMessage(message, "*")
+  }
+}
+
+interface NavigateOptions {
+  component: string
+}
+
+export function navigateTo({ component }: NavigateOptions) {
+  if (component) {
+    sendMessage({
+      source: SOURCE_NAME,
+      event: "navigateTo",
+      payload: { component },
+    })
+  }
+}
+
+export function navigateBack() {
+  sendMessage(NAVIGATE_BACK_MESSAGE)
+}
+
+
+export function framedWrapper() {
+  return process.env.TARO_ENV === "h5" && parent != window
+}
