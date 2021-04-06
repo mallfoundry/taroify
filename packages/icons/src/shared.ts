7e7e5384aba57bf58b9928f19c66b8cbb38aed59
TangZhi <tgioer@163.com>
2021-04-06 15:10:13 +0800

    合并 IconProps 属性。

--- packages/icons/src/shared.ts
+++ packages/icons/src/shared.ts
@@ -1,9 +1,33 @@
 import { CSSProperties } from "react"
-import { IconColor, IconColorString, IconSize, IconSizeString } from "./Icon"
 
-export interface NamedIconProps {
+export enum IconSize {
+  Inherit = "inherit",
+  Mini = "mini",
+  Small = "small",
+  Medium = "medium",
+  Large = "large",
+}
+
+export type IconSizeString = "inherit" | "mini" | "small" | "medium" | "large"
+
+export enum IconColor {
+  Inherit = "inherit",
+  Default = "default",
+  Primary = "primary",
+  Info = "info",
+  Success = "success",
+  Warning = "warning",
+  Danger = "danger"
+}
+
+export type IconColorString = "inherit" | "default" | "primary" | "info" | "success" | "warning" | "danger"
+
+export interface IconProps {
   className?: string
   style?: CSSProperties
   size?: IconSize | IconSizeString
   color?: IconColor | IconColorString
+  children?: string
+  // events
+  onClick?: () => void
 }
