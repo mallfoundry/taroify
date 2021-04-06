b677f1e60c5bee28663af3f873084261e4b8e10a
TangZhi <tgioer@163.com>
2021-04-06 15:21:40 +0800

    添加 Vant 图标。

---
+++ packages/icons/src/vant/VantIcon.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { CSSProperties } from "react"
+import classNames from "classnames"
+import { View } from "@tarojs/components"
+import { IconColor, IconColorString, IconSize, IconSizeString } from "../shared"
+
+interface VantIconProps {
+  className?: string
+  style?: CSSProperties
+  size?: IconSize | IconSizeString
+  color?: IconColor | IconColorString
+  children?: string
+  onClick?: () => void
+}
+
+export default function VantIcon(props: VantIconProps) {
+  const {
+    className, style,
+    size = IconSize.Medium,
+    color = IconColor.Inherit,
+    children,
+    onClick,
+  } = props
+
+  return (
+    <View
+      className={
+        classNames(
+          `van-icon`,
+          `van-icon-${children}`,
+          `taroify-icon`,
+          {
+            [`taroify-icon-${color}`]: color,
+            [`taroify-icon-${size}`]: size,
+          },
+          className,
+        )}
+      style={style}
+      onClick={onClick}
+    />
+  )
+}
---
+++ packages/icons/src/vant/index.ts
@@ -0,0 +1,1 @@
+export { default as VantIcon } from "./VantIcon"
