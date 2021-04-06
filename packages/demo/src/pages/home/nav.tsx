7bdde567496dd164e9e8eeee87fe27ef71ae671b
TangZhi <tgioer@163.com>
2021-04-06 14:55:15 +0800

    添加 framedWrapper 组件。

--- packages/demo/src/pages/home/nav.tsx
+++ packages/demo/src/pages/home/nav.tsx
@@ -2,19 +2,34 @@ import * as React from "react"
 import { ReactNode } from "react"
 import { Navigator, View } from "@tarojs/components"
 import ArrowRight from "@taroify/icons/ArrowRight"
+import { framedWrapper, navigateTo } from "../../utils/framed-router"
 import classes from "./nav.module.scss"
 
 interface NavBlockProps {
   title?: ReactNode
+  component?: string
   href?: string
 }
 
 export function NavBlock(props: NavBlockProps) {
-  const { title, href } = props
+  const { title, component, href } = props
+  if (framedWrapper()) {
+    function handleClick() {
+      if (component) {
+        navigateTo({ component })
+      }
+    }
+
+    return (
+      <View className={classes.NavBlock} onClick={handleClick}>
+        {title}
+        <ArrowRight size="small" />
+      </View>
+    )
+  }
+
   return (
-    <Navigator
-      className={classes.NavBlock}
-      url={href}>
+    <Navigator className={classes.NavBlock} url={href}>
       {title}
       <ArrowRight size="small" />
     </Navigator>
