bc35253119a1becd8c17048f515239b908389ed6
TangZhi <tgioer@163.com>
2021-04-06 15:00:29 +0800

    添加 ArrowDown 图标。

---
+++ packages/icons/src/ArrowDown.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { IconProps } from "./shared"
+import { VantIcon } from "./vant"
+
+export default function ArrowDown(props: IconProps) {
+  const { ...rest } = props
+  return (
+    <VantIcon children="arrow-down" {...rest} />
+  )
+}
