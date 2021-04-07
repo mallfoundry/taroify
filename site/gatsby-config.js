478956a25b12faea5126defb065bb10205fd965c
TangZhi <tgioer@163.com>
2021-04-07 12:26:59 +0800

    添加 siteUrl 属性

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,90 +1,91 @@
 module.exports = {
-    siteMetadata: {
-        title: "My Gatsby Site",
+  siteMetadata: {
+    title: "Taroify",
+    siteUrl: `https://www.taroify.com`,
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
     },
-    plugins: [
-        "gatsby-plugin-sass",
-        "gatsby-plugin-image",
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sitemap",
-        {
-            resolve: `gatsby-plugin-typescript`,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: `Taroify`,
+        short_name: `Taroify`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#4fc08d`,
+        display: `minimal-ui`,
+        icon: "src/images/icon.png",
+        theme_color_in_head: false,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "content",
+        path: "./content/",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                isTSX: true, // defaults to false
-                jsxPragma: `jsx`, // defaults to "React"
-                allExtensions: true, // defaults to false
+              maxWidth: 590,
             },
-        },
-        {
-            resolve: "gatsby-plugin-manifest",
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-                name: `Vant Taro`,
-                short_name: `Vant Taro`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#4fc08d`,
-                display: `minimal-ui`,
-                icon: "src/images/icon.png",
-                theme_color_in_head: false,
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
-        },
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-        {
-            resolve: "gatsby-source-filesystem",
+          },
+          "gatsby-remark-autolink-headers",
+          {
+            resolve: "gatsby-remark-prismjs",
             options: {
-                name: "images",
-                path: "./src/images/",
+              inlineCodeMarker: "÷",
             },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
+          },
+          "gatsby-remark-smartypants",
+          {
+            resolve: "gatsby-remark-external-links",
             options: {
-                name: "pages",
-                path: "./src/pages/",
+              target: "_blank",
             },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "content",
-                path: "./content/",
-            },
-        },
-        {
-            resolve: 'gatsby-transformer-remark',
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    'gatsby-remark-autolink-headers',
-                    {
-                        resolve: 'gatsby-remark-prismjs',
-                        options: {
-                            // classPrefix: 'gatsby-code-',
-                            inlineCodeMarker: '÷',
-                        },
-                    },
-                    'gatsby-remark-smartypants',
-                    {
-                        resolve: 'gatsby-remark-external-links',
-                        options: {
-                            target: '_blank',
-                        },
-                    },
-                ]
-            }
-        },
-    ],
+          },
+        ],
+      },
+    },
+  ],
 
-};
+}
