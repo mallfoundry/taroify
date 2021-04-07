0fd2911d7cb6a171fd0099205dbc9951ab4cdba6
TangZhi <tgioer@163.com>
2021-04-07 12:06:10 +0800

    待补充...

--- packages/demo/src/pages/icon/index.tsx
+++ packages/demo/src/pages/icon/index.tsx
@@ -1,17 +1,13 @@
 import * as React from "react"
 import Block from "../../components/block"
 import Page from "../../components/page"
-import ArrowBackIosOutlined from "@taroify/icons/ArrowBackIosOutlined"
-import ArrowForwardIosOutlined from "@taroify/icons/ArrowForwardIosOutlined"
 import "./index.scss"
 
 export default function IconDemo() {
   return (
     <Page title="图标">
       <Block title="按钮类型">
-        <ArrowBackIosOutlined />
-        <ArrowBackIosOutlined />
-        <ArrowForwardIosOutlined />
+        待补充...
       </Block>
     </Page>
   )
