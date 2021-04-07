41a4fe979dd035c8e38b0c3692f09e1e534d473d
TangZhi <tgioer@163.com>
2021-04-07 11:15:33 +0800

    添加 taroify-icon 前缀支持。

--- packages/icons/src/material/MaterialIcon.tsx
+++ packages/icons/src/material/MaterialIcon.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames"
 import { View } from "@tarojs/components"
 import * as React from "react"
 import { CSSProperties } from "react"
+import { IconColor, IconColorString, IconSize, IconSizeString } from "../shared"
 
 export enum MaterialIconTheme {
   Filled = "filled",
@@ -16,11 +17,19 @@ interface MaterialIconProps {
   className?: string
   style?: CSSProperties
   theme?: MaterialIconTheme | MaterialIconThemeString
+  size?: IconSize | IconSizeString
+  color?: IconColor | IconColorString
   children?: string
 }
 
 export default function MaterialIcon(props: MaterialIconProps) {
-  const { className, style, theme = MaterialIconTheme.Filled, children } = props
+  const {
+    className, style,
+    theme = MaterialIconTheme.Filled,
+    size = IconSize.Medium,
+    color = IconColor.Inherit,
+    children,
+  } = props
   return (
     <View
       className={
@@ -28,6 +37,11 @@ export default function MaterialIcon(props: MaterialIconProps) {
           {
             ["material-icons"]: theme === MaterialIconTheme.Filled,
             [`material-icons-${theme}`]: theme !== MaterialIconTheme.Filled,
+          },
+          `taroify-icon`,
+          {
+            [`taroify-icon-${color}`]: color,
+            [`taroify-icon-${size}`]: size,
           },
           className,
         )}
