9296e6a7debe41fccd78c8f89d55e983ac18061d
TangZhi <tgioer@163.com>
2021-04-10 13:24:26 +0800

    采用 bem 命名规则

--- packages/core/src/loading/loading.tsx
+++ packages/core/src/loading/loading.tsx
@@ -1,16 +1,17 @@
 import { View } from "@tarojs/components"
 import * as _ from "lodash"
 import * as React from "react"
+import classNames from "classnames"
 import { prefixClassname } from "../styles"
 
 const SpinIcon = _.range(0, 12)
   .map(key => (
-    <View key={key} className={prefixClassname("loading-spinner-item")} />
+    <View key={key} className={prefixClassname("loading__spinner__item")} />
   ))
 
 function LoadingSpinner() {
   return (
-    <View className={prefixClassname("loading-spinner")}>
+    <View className={prefixClassname("loading__spinner")}>
       {SpinIcon}
     </View>
   )
@@ -18,10 +19,10 @@ function LoadingSpinner() {
 
 function LoadingCircular() {
   return (
-    <View className={prefixClassname("loading-circular")}>
-      <View className={prefixClassname("loading-circular-item")} />
-      <View className={prefixClassname("loading-circular-item")} />
-      <View className={prefixClassname("loading-circular-item")} />
+    <View className={prefixClassname("loading__circular")}>
+      <View className={prefixClassname("loading__circular__item")} />
+      <View className={prefixClassname("loading__circular__item")} />
+      <View className={prefixClassname("loading__circular__item")} />
     </View>
   )
 }
@@ -43,7 +44,10 @@ export default function Loading(props: LoadingProps) {
   const { type = LoadingType.Circular } = props
 
   return (
-    <View className={prefixClassname("loading")}>
+    <View className={classNames(prefixClassname("loading"), {
+      [prefixClassname("loading--spinner")] :type === LoadingType.Spinner,
+      [prefixClassname("loading--circular")] :type === LoadingType.Circular,
+    })}>
       {type === LoadingType.Spinner && <LoadingSpinner />}
       {type === LoadingType.Circular && <LoadingCircular />}
     </View>
