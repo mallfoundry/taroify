bb027c820241eb24f6cbcfa48b7b4812528db526
TangZhi <tgioer@163.com>
2021-04-10 13:08:09 +0800

    采用 bem 命名规则

--- packages/core/src/cell/cell.tsx
+++ packages/core/src/cell/cell.tsx
@@ -5,9 +5,28 @@ import classNames from "classnames"
 import { prefixClassname } from "../styles"
 import { ITouchEvent } from "@tarojs/components/types/common"
 
+export enum CellSize {
+  Medium = "medium",
+  Large = "large",
+}
+
+type CellSizeString = "medium" | "large"
+
+enum CellAlign {
+  Start = "start",
+  Center = "center",
+  End = "end"
+}
+
+type CellAlignString = "start" | "center" | "end"
+
 interface CellProps {
   className?: string
+  size?: CellSize | CellSizeString
+  align?: CellAlign | CellAlignString
   label?: ReactNode
+  description?: ReactNode
+  startIcon?: ReactNode
   endIcon?: ReactNode
   bordered?: boolean
   clickable?: boolean
@@ -16,23 +35,52 @@ interface CellProps {
 }
 
 export default function Cell(props: CellProps) {
-  const { className, label, clickable = false, bordered = true, endIcon, children, onClick } = props
+  const {
+    className,
+    size = CellSize.Medium,
+    align,
+    label,
+    description,
+    clickable = false,
+    bordered = true,
+    startIcon,
+    endIcon,
+    children,
+    onClick,
+  } = props
+
   return (
     <View
       className={classNames(
         prefixClassname("cell"),
         {
-          [prefixClassname(`cell-clickable`)]: clickable,
-          [prefixClassname(`cell-borderless`)]: !bordered,
+          [prefixClassname(`cell--start`)]: align === CellAlign.Start,
+          [prefixClassname(`cell--center`)]: align === CellAlign.Center,
+          [prefixClassname(`cell--end`)]: align === CellAlign.End,
+          [prefixClassname(`cell--large`)]: size === CellSize.Large,
+          [prefixClassname(`cell--clickable`)]: clickable,
+          [prefixClassname(`cell--borderless`)]: !bordered,
         },
         className,
       )}
       onClick={onClick}>
-      <View className={prefixClassname("cell-label")}>{label}</View>
-      <View className={prefixClassname("cell-value")}>{children}</View>
+      {
+        startIcon && (
+          <View className={prefixClassname("cell-start-icon")}>
+            {startIcon}
+          </View>
+        )
+      }
+      {label && <View className={prefixClassname("cell__label")}>
+        {label}
+        {description && <View className={prefixClassname("cell__description")} children={description} />}
+      </View>}
+      <View className={classNames(prefixClassname("cell__value"), {
+        [prefixClassname("cell__value--alone")]: !label,
+      })}>{children}</View>
       {
         endIcon && (
-          <View className={prefixClassname("cell-end-icon")}>
+          <View className={prefixClassname("cell__end-icon")}>
             {endIcon}
           </View>
         )
