7937fde1c760d293474c37b374ce6936b0fca004
TangZhi <tgioer@163.com>
2021-04-11 18:41:35 +0800

    合并为 document-template.tsx

--- site/src/templates/document-template.tsx
+++ site/src/templates/document-template.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import * as _ from "lodash"
 import { ReactNode } from "react"
 import Handler from "../components/header"
 import SideNav from "../components/side-nav"
@@ -15,6 +16,7 @@ export default function ComponentTemplate(props: ComponentTemplateProps) {
   const { data } = props
   const post = data.markdownRemark
   const { slug } = post.fields
+
   return (
     <div className="vant-docs">
       <Handler />
@@ -22,7 +24,7 @@ export default function ComponentTemplate(props: ComponentTemplateProps) {
       <div className="vant-docs-container vant-docs-container-with-simulator">
         <div className="vant-docs-content" dangerouslySetInnerHTML={{ __html: docsCardWrapper(post.html) }} />
       </div>
-      <Simulator />
+      <Simulator slug={_.startsWith(slug, "/components") ? slug : undefined} />
     </div>
   )
 }
