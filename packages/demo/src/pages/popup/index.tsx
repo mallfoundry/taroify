e2be4a6c5eaf0363d44af4e0f8afcc570b2f606e
TangZhi <tgioer@163.com>
2021-04-12 15:44:56 +0800

    使用 PopupPlacement 替换 PopupAnchor

--- packages/demo/src/pages/popup/index.tsx
+++ packages/demo/src/pages/popup/index.tsx
@@ -1,33 +1,35 @@
 import * as React from "react"
 import { CSSProperties, useState } from "react"
 import Cell from "@taroify/core/cell"
-import Popup, { PopupAnchor } from "@taroify/core/popup"
+import Popup, { PopupPlacement } from "@taroify/core/popup"
 import ArrowRight from "@taroify/icons/ArrowRight"
 import Page from "../../components/page"
 import Block from "../../components/block"
 
 interface OpenOptions {
   open?: boolean,
+  closeable?: boolean
   style?: CSSProperties
-  anchor?: PopupAnchor
+  placement?: PopupPlacement
   rounded?: boolean
 }
 
 export default function PopupDemo() {
   const [options, setOptions] = useState<OpenOptions>({})
 
-  function handleOpen({ anchor, rounded }: OpenOptions) {
+  function handleOpen({ placement, rounded, closeable }: OpenOptions) {
     const openOptions: OpenOptions = {
       open: true,
-      anchor,
+      placement,
       rounded,
+      closeable
     }
-    if (anchor === PopupAnchor.Left || anchor === PopupAnchor.Right) {
+    if (placement === PopupPlacement.Left || placement === PopupPlacement.Right) {
       openOptions.style = {
         height: "20%",
         width: "20%",
       }
-    } else if (anchor) {
+    } else if (placement) {
       openOptions.style = { height: "30%" }
 
     } else {
@@ -44,38 +46,57 @@ export default function PopupDemo() {
       <Block title="基础用法">
         <Cell clickable label="显示遮盖层"
               endIcon={<ArrowRight />}
-              onClick={() => handleOpen({ anchor: undefined })}
+              onClick={() => handleOpen({ placement: undefined })}
         />
       </Block>
       <Block title="弹出位置">
         <Cell clickable label="顶部弹出"
               endIcon={<ArrowRight />}
-              onClick={() => handleOpen({ anchor: PopupAnchor.Top })}
+              onClick={() => handleOpen({ placement: PopupPlacement.Top })}
         />
         <Cell clickable label="底部弹出"
               endIcon={<ArrowRight />}
-              onClick={() => handleOpen({ anchor: PopupAnchor.Bottom })}
+              onClick={() => handleOpen({ placement: PopupPlacement.Bottom })}
         />
         <Cell clickable label="左侧弹出"
               endIcon={<ArrowRight />}
-              onClick={() => handleOpen({ anchor: PopupAnchor.Left })}
+              onClick={() => handleOpen({ placement: PopupPlacement.Left })}
         />
         <Cell clickable label="右侧弹出"
               endIcon={<ArrowRight />}
-              onClick={() => handleOpen({ anchor: PopupAnchor.Right })}
+              onClick={() => handleOpen({ placement: PopupPlacement.Right })}
+        />
+      </Block>
+      <Block title="关闭按钮">
+        <Cell clickable label="顶部弹出"
+              endIcon={<ArrowRight />}
+              onClick={() => handleOpen({ placement: PopupPlacement.Top, closeable: true })}
+        />
+        <Cell clickable label="底部弹出"
+              endIcon={<ArrowRight />}
+              onClick={() => handleOpen({ placement: PopupPlacement.Bottom, closeable: true })}
+        />
+        <Cell clickable label="左侧弹出"
+              endIcon={<ArrowRight />}
+              onClick={() => handleOpen({ placement: PopupPlacement.Left, closeable: true })}
+        />
+        <Cell clickable label="右侧弹出"
+              endIcon={<ArrowRight />}
+              onClick={() => handleOpen({ placement: PopupPlacement.Right, closeable: true })}
         />
       </Block>
       <Block title="圆角弹窗">
         <Cell clickable label="圆角弹窗"
               endIcon={<ArrowRight />}
-              onClick={() => handleOpen({ anchor: PopupAnchor.Bottom, rounded: true })}
+              onClick={() => handleOpen({ placement: PopupPlacement.Bottom, rounded: true })}
         />
       </Block>
       <Popup
         open={options.open}
-        anchor={options.anchor}
+        placement={options.placement}
         rounded={options.rounded}
         style={options.style}
+        closeable={options.closeable}
         onClose={() => setOptions({
           ...options,
           open: false,
