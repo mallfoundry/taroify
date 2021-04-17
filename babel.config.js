f385e920a523d802936794797ff8e4340da752f1
TangZhi <tgioer@163.com>
2021-04-17 12:47:08 +0800

    format code

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,8 @@ module.exports = {
   presets: [
     ["@babel/preset-env", {
       targets: {
-        esmodules: true
-      }
+        esmodules: true,
+      },
     }],
     "@babel/preset-react",
     "@babel/preset-typescript",
