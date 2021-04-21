14b9ca97340df7ae913b22af478efa805c9af65d
TangZhi <tgioer@163.com>
2021-04-21 13:50:26 +0800

    Add @taroify/babel-preset package

---
+++ packages/babel-preset/index.js
@@ -0,0 +1,26 @@
+module.exports = () => {
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          // targets: {
+          //   esmodules: true,
+          // },
+        },
+      ],
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+    ],
+    plugins: [
+      [
+        "@babel/plugin-transform-typescript",
+        {
+          sourceMaps: "both",
+        },
+      ],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ],
+  }
+}
