3520b290cd7a1231526a807b27c8a4986a067156
TangZhi <tgioer@163.com>
2021-04-21 19:12:27 +0800

    Add '$demo-prefix' variable

---
+++ packages/demo/src/styles/index.ts
@@ -0,0 +1,1 @@
+export { demoPrefix } from "./prefix"
---
+++ packages/demo/src/styles/prefix.ts
@@ -0,0 +1,5 @@
+const DEMO_PREFIX = "taroify-demo-"
+
+export function demoPrefixClassname(component: string) {
+  return `${DEMO_PREFIX}${component}`
+}
