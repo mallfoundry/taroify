70fd174e8ecae9b72e9bb7c6bfbf58eddc9204f8
TangZhi <tgioer@163.com>
2021-04-21 13:51:20 +0800

    Add @taroify/stylelint-config package

---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-sass-guidelines",
+    "stylelint-config-recess-order",
+    "stylelint-config-prettier",
+  ],
+  rules: {
+    "selector-type-no-unknown": [
+      true,
+      {
+        ignoreTypes: ["/^taro-/"],
+      },
+    ],
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["host", "global"],
+      },
+    ],
+    "max-nesting-depth": [
+      3,
+      {
+        ignoreAtRules: ["each", "media", "supports", "include"],
+      },
+    ],
+  },
+}
