bd5de3a36481ec412cd13733138632a72516e26d
TangZhi <tgioer@163.com>
2021-04-22 15:12:23 +0800

    Export Navbar component

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,17 @@
-export { default as Backdrop } from "./backdrop"
+// Basic Components
 export { default as Button } from "./button"
 export { default as Cell } from "./cell"
 export { default as CellGroup } from "./cell-group"
 export { default as Col } from "./col"
 export { default as Image } from "./image"
-export { default as Loading } from "./loading"
 export { default as Popup } from "./popup"
 export { default as Row } from "./row"
 export { default as Space } from "./space"
 export { default as Toast } from "./toast"
 export { default as Transition } from "./transition"
 export { default as WhiteSpace } from "./white-space"
+// Action Components
+export { default as Backdrop } from "./backdrop"
+export { default as Loading } from "./loading"
+// Navigation Components
+export { default as Navbar } from "./navbar"
