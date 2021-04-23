a7b45fd1e97be1d9c55afd62b437d434ea024d88
TangZhi <tgioer@163.com>
2021-04-23 12:00:43 +0800

    Add FriendsOutlined, HomeOutlined, SettingOutlined icon components

---
+++ packages/icons/src/FriendsOutlined.tsx
@@ -0,0 +1,4 @@
+import { createVanIconComponent } from "./van"
+
+const FriendsOutlined = createVanIconComponent("friends-o")
+export default FriendsOutlined
---
+++ packages/icons/src/HomeOutlined.tsx
@@ -0,0 +1,4 @@
+import { createVanIconComponent } from "./van"
+
+const HomeOutlined = createVanIconComponent("home-o")
+export default HomeOutlined
