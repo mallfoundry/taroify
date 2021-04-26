c5f191cf58e7c8ef87752b2cebe6886a646cd90c
TangZhi <tgioer@163.com>
2021-04-26 14:00:49 +0800

    Add hairline variables

--- packages/core/src/styles/hairline.ts
+++ packages/core/src/styles/hairline.ts
@@ -1,5 +1,9 @@
 import { prefixClassname } from "./prefix"
 
-const HAIRLINE = prefixClassname("hairline")
-
-export const HAIRLINE_BOTTOM = `${HAIRLINE}--bottom`
+export const HAIRLINE_BORDER = prefixClassname("hairline")
+export const HAIRLINE_BORDER_TOP = `${HAIRLINE_BORDER}--top`
+export const HAIRLINE_BORDER_LEFT = `${HAIRLINE_BORDER}--left`
+export const HAIRLINE_BORDER_BOTTOM = `${HAIRLINE_BORDER}--bottom`
+export const HAIRLINE_BORDER_SURROUND = `${HAIRLINE_BORDER}--surround`
+export const HAIRLINE_BORDER_TOP_BOTTOM = `${HAIRLINE_BORDER}--top-bottom`
+export const HAIRLINE_BORDER_UNSET_TOP_BOTTOM = `${HAIRLINE_BORDER}-unset--top-bottom`
