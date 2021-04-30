1be7181703d30493c26825cd49c9bb2809ba38c9
TangZhi <tgioer@163.com>
2021-04-30 10:57:41 +0800

    eslint --fix

--- packages/gulp/readme.js
+++ packages/gulp/readme.js
@@ -3,20 +3,28 @@ const { watch } = require("gulp")
 const rename = require("gulp-rename")
 
 function copyReadmeFiles(directory) {
-  const copyReadmeFilesTask = () => gulp.src([`./packages/${directory}/**/README*.md?(x)`], {})
-    .pipe(rename((path) => {
-      path.basename = "index"
-    }))
-    .pipe(gulp.dest(`./site/.content/components`))
+  const copyReadmeFilesTask = () =>
+    gulp
+      .src([`./packages/${directory}/**/README*.md?(x)`], {})
+      .pipe(
+        rename((path) => {
+          path.basename = "index"
+        }),
+      )
+      .pipe(gulp.dest("./site/.content/components"))
   copyReadmeFilesTask.displayName = `copy README.md?(x) files to site/.contents/components from packages/${directory}`
   return copyReadmeFilesTask
 }
 
 function watchReadmeFiles(directory) {
-  watch([`./packages/${directory}/**/README*.md?(x)`], {
-    // events: "all",
-    ignoreInitial: false,
-  }, copyReadmeFiles(directory))
+  watch(
+    [`./packages/${directory}/**/README*.md?(x)`],
+    {
+      // events: "all",
+      ignoreInitial: false,
+    },
+    copyReadmeFiles(directory),
+  )
 }
 
 exports.copyReadmeFiles = copyReadmeFiles
