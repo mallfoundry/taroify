f2357ff9c601c05395815062e576fc54add9dc33
TangZhi <tgioer@163.com>
2021-05-21 11:57:01 +0800

    Add custom demos

--- packages/demo/src/pages/loading/index.tsx
+++ packages/demo/src/pages/loading/index.tsx
@@ -13,6 +13,18 @@ export default function LoadingDemo() {
           <Loading type="spinner" />
         </Space>
       </Block>
+      <Block title="自定义颜色">
+        <Space>
+          <Loading color="#1989fa" />
+          <Loading type="spinner" color="#1989fa" />
+        </Space>
+      </Block>
+      <Block title="自定义大小">
+        <Space>
+          <Loading size="24" />
+          <Loading type="spinner" size="24" />
+        </Space>
+      </Block>
     </Page>
   )
 }
