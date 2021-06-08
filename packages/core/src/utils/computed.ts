22e36b01c7570833e5afab37ee731cde2a8d0530
TangZhi <tgioer@163.com>
2021-06-08 16:20:15 +0800

    Add a useComputed hook

---
+++ packages/core/src/utils/computed.ts
@@ -0,0 +1,26 @@
+import { DependencyList, useCallback, useMemo } from "react"
+
+export interface ComputedRef<T = any> {
+  readonly value: T
+}
+
+class GetterRef<T> implements ComputedRef<T> {
+  private readonly getter: ComputedGetter<any>
+
+  constructor(getter: ComputedGetter<any>) {
+    this.getter = getter
+  }
+
+  get value(): T {
+    return this.getter()
+  }
+}
+
+type ComputedGetter<T> = () => T
+
+export function useComputed<T>(getter: ComputedGetter<T>, deps: DependencyList): ComputedRef<T> {
+  const __getter__ = useCallback(getter, [getter, ...deps])
+  return useMemo(() => {
+    return new GetterRef(__getter__)
+  }, [__getter__])
+}
