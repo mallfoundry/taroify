69c5bb4bc7ab1adad0b7f8e80ffc4813b3f37cab
TangZhi <tgioer@163.com>
2021-06-09 13:47:19 +0800

    Add a demo of Swiper.Indicator component

--- packages/demo/src/pages/swiper/index.tsx
+++ packages/demo/src/pages/swiper/index.tsx
@@ -1,13 +1,22 @@
 import { Swiper } from "@taroify/core"
 import * as React from "react"
+import { useState } from "react"
 import Block from "../../components/block"
 import Page from "../../components/page"
 import "./index.scss"
 
 function BasicSwiper() {
+  const [activeIndex, setActiveIndex] = useState(0)
   return (
     <Block title="基础用法">
-      <Swiper className="basic-swiper">
+      <Swiper
+        className="basic-swiper"
+        activeIndex={activeIndex}
+        autoplay={3000}
+        width={300}
+        onChange={(event) => setActiveIndex(event.index)}
+      >
+        <Swiper.Indicator />
         <Swiper.Item>1</Swiper.Item>
         <Swiper.Item>2</Swiper.Item>
         <Swiper.Item>3</Swiper.Item>
@@ -21,6 +30,7 @@ function VerticalSwiper() {
   return (
     <Block title="纵向滚动">
       <Swiper className="vertical-swiper" direction="vertical">
+        <Swiper.Indicator />
         <Swiper.Item>1</Swiper.Item>
         <Swiper.Item>2</Swiper.Item>
         <Swiper.Item>3</Swiper.Item>
