150083f06162a95929a8f4e60a135ea290444ed3
TangZhi <tgioer@163.com>
2021-06-11 14:23:22 +0800

    Add customSize setting

--- packages/core/src/white-space/white-space.tsx
+++ packages/core/src/white-space/white-space.tsx
@@ -1,6 +1,7 @@
 import { View } from "@tarojs/components"
 import classNames from "classnames"
 import * as React from "react"
+import { CSSProperties, useMemo } from "react"
 import { prefixClassname } from "../styles"
 
 export enum WhiteSpaceSize {
@@ -10,20 +11,37 @@ export enum WhiteSpaceSize {
   Large = "large",
 }
 
+const SIZE_PRESETS = [
+  WhiteSpaceSize.Mini,
+  WhiteSpaceSize.Small,
+  WhiteSpaceSize.Medium,
+  WhiteSpaceSize.Large,
+]
+
+function isPresetSize(size?: WhiteSpaceSize | string | number) {
+  return SIZE_PRESETS.includes(size as WhiteSpaceSize)
+}
+
 type WhiteSpaceSizeString = "mini" | "small" | "medium" | "large"
 
 interface WhiteSpaceProps {
-  size?: WhiteSpaceSize | WhiteSpaceSizeString
+  size?: WhiteSpaceSize | WhiteSpaceSizeString | string | number
 }
 
 export default function WhiteSpace(props: WhiteSpaceProps) {
   const { size = WhiteSpaceSize.Medium } = props
+
+  const rootStyle = useMemo<CSSProperties>(
+    () => ({ height: isPresetSize(size) ? "" : size }), //
+    [size],
+  )
+
   return (
     <View
-      className={classNames(
-        prefixClassname("white-space"),
-        prefixClassname(`white-space-size-${size}`),
-      )}
+      className={classNames(prefixClassname("white-space"), {
+        [prefixClassname(`white-space-size-${size}`)]: isPresetSize(size),
+      })}
+      style={rootStyle}
     />
   )
 }
