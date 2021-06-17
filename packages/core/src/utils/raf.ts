57fa78362b7107358823c9980d5e327b8a30a459
TangZhi <tgioer@163.com>
2021-06-17 17:23:13 +0800

    Export cancelRaf function

--- packages/core/src/utils/raf.ts
+++ packages/core/src/utils/raf.ts
@@ -1,9 +1,22 @@
+import * as _ from "lodash"
 import * as requestAnimationFrame from "raf"
 
-export function raf(cb: FrameRequestCallback) {
-  requestAnimationFrame(cb)
+export function cancelRaf(rafId: number | number[]) {
+  if (_.isNumber(rafId)) {
+    requestAnimationFrame.cancel(rafId)
+  } else if (_.isArray(rafId)) {
+    _.forEach(rafId, cancelRaf)
+  }
 }
 
-export function doubleRaf(cb: FrameRequestCallback) {
-  requestAnimationFrame(() => requestAnimationFrame(cb))
+export function raf(cb: FrameRequestCallback) {
+  return requestAnimationFrame(cb)
+}
+
+export function doubleRaf(cb: FrameRequestCallback): [number, number] {
+  const rafIds: [number, number] = [0, 0]
+  rafIds[1] = requestAnimationFrame(() => {
+    rafIds[0] = requestAnimationFrame(cb)
+  })
+  return rafIds
 }
