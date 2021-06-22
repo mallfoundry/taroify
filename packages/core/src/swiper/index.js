45a241c9cc9b142c494580a47db3975ccd2e8766
TangZhi <tgioer@163.com>
2021-06-22 14:30:22 +0800

    Set allowJs to 'false'

--- packages/core/src/swiper/index.js
+++
@@ -1,8 +0,0 @@
-import Swiper from "./swiper"
-import SwiperIndicator from "./swiper-indicator"
-import SwiperItem from "./swiper-item"
-
-Swiper.Item = SwiperItem
-Swiper.Indicator = SwiperIndicator
-
-export default Swiper
---
+++ packages/core/src/swiper/index.ts
@@ -0,0 +1,27 @@
+import SwiperComponent, { SwiperProps } from "./swiper"
+import SwiperIndicator, { SwiperIndicatorProps } from "./swiper-indicator"
+import SwiperItem, { SwiperItemProps } from "./swiper-item"
+import { SwiperItemEvent } from "./swiper.shared"
+
+interface SwiperInterface {
+  (props: SwiperProps): JSX.Element
+
+  Item: typeof SwiperItem
+  Indicator: typeof SwiperIndicator
+}
+
+const Swiper = (SwiperComponent as unknown) as SwiperInterface
+
+Swiper.Item = SwiperItem
+Swiper.Indicator = SwiperIndicator
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+namespace Swiper {
+  export type ItemEvent = SwiperItemEvent
+
+  export type ItemProps = SwiperItemProps
+
+  export type IndicatorProps = SwiperIndicatorProps
+}
+
+export default Swiper
