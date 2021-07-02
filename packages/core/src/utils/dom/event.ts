f1b5a3a8c6a5a3c639f1a397672f32d8eb4a61d5
tangzhi <tgioer@163.com>
2021-07-02 13:55:17 +0800

    Fix event is cancelable

--- packages/core/src/utils/dom/event.ts
+++ packages/core/src/utils/dom/event.ts
@@ -1,11 +1,19 @@
 import { CommonEvent } from "@tarojs/components"
+import { inBrowser } from "../base"
 
 export function stopPropagation(event: CommonEvent) {
   event.stopPropagation()
 }
 
 export function preventDefault(event: CommonEvent, isStopPropagation?: boolean) {
-  event.preventDefault()
+  if (inBrowser) {
+    if (event.preventDefault && ((event as unknown) as TouchEvent).cancelable) {
+      event.preventDefault()
+    }
+  } else {
+    event.preventDefault()
+  }
+
   if (isStopPropagation) {
     stopPropagation(event)
   }
