3ed81b11f270a04701df40189130d5d987aab1dc
tangzhi <tgioer@163.com>
2021-07-02 15:51:37 +0800

    Rename ShareSheetContextProps to ShareSheetContextValue

--- packages/core/src/share-sheet/share-sheet.context.ts
+++ packages/core/src/share-sheet/share-sheet.context.ts
@@ -1,10 +1,10 @@
 import { createContext } from "react"
 import ShareSheet from "./share-sheet"
 
-interface ShareSheetContextProps {
+interface ShareSheetContextValue {
   emitSelect?: (event: ShareSheet.OptionEvent) => void
 }
 
-const ShareSheetContext = createContext<ShareSheetContextProps>({})
+const ShareSheetContext = createContext<ShareSheetContextValue>({})
 
 export default ShareSheetContext
