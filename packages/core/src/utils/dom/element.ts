86a161458c2bb43ad4c8cbfc93bc412e10f265f9
tangzhi <tgioer@163.com>
2021-07-02 13:54:49 +0800

    Add a element file

---
+++ packages/core/src/utils/dom/element.ts
@@ -0,0 +1,27 @@
+import { createSelectorQuery } from "@tarojs/taro"
+
+export const ELEMENT_NODE_TYPE = 1
+
+export interface TaroElement extends HTMLElement {
+  uid: string
+}
+
+export function elementUnref(elementOrRef: any): TaroElement {
+  if (elementOrRef === undefined || elementOrRef === null) {
+    return elementOrRef
+  }
+  if ("current" in elementOrRef) {
+    return elementOrRef.current
+  }
+  return elementOrRef
+}
+
+export function isRootElement(node: TaroElement) {
+  return node.nodeType === ELEMENT_NODE_TYPE && node.tagName === "ROOT"
+}
+
+export function createNodesRef(element: TaroElement) {
+  return isRootElement(element)
+    ? createSelectorQuery().selectViewport()
+    : createSelectorQuery().select("#" + element.uid)
+}
