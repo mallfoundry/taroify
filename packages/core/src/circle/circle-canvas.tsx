8720c2f43f055133773f4987242b5579379eab10
tangzhi <tgioer@163.com>
2021-07-08 18:05:57 +0800

    Add a weapp circle component

---
+++ packages/core/src/circle/circle-canvas.tsx
@@ -0,0 +1,9 @@
+import { View } from "@tarojs/components"
+import * as React from "react"
+import { CircleProps } from "./circle.shared"
+
+function CircleCanvas(props: CircleProps) {
+  return <View>weapp</View>
+}
+
+export default CircleCanvas
