dade54878270bfc4d4c9d29211aa345141042cc8
tangzhi <tgioer@163.com>
2021-07-14 11:34:39 +0800

    Fix './shared' to './popup.shared'

--- packages/core/src/popup/popup.context.ts
+++ packages/core/src/popup/popup.context.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react"
-import { PopupPlacement, PopupPlacementString } from "./shared"
+import { PopupPlacement, PopupPlacementString } from "./popup.shared"
 
 interface PopupContextProps {
   open?: boolean
