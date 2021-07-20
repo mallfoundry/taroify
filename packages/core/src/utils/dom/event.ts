7b6a6e9a0216a52c88626e61c17bedc3bdc1be16
Tang Zhi <tgioer@163.com>
2021-07-20 21:04:22 +0800

    Add the getClientCoordinates function

--- packages/core/src/utils/dom/event.ts
+++ packages/core/src/utils/dom/event.ts
@@ -1,4 +1,4 @@
-import { CommonEvent } from "@tarojs/components"
+import { CommonEvent, ITouchEvent } from "@tarojs/components"
 import { inBrowser } from "../base"
 
 export function stopPropagation(event: CommonEvent) {
@@ -17,4 +17,22 @@ export function preventDefault(event: CommonEvent, isStopPropagation?: boolean) 
   if (isStopPropagation) {
     stopPropagation(event)
   }
+}
+
+interface ClientCoordinates {
+  clientX: number
+  clientY: number
+}
+
+export function getClientCoordinates(event: ITouchEvent | MouseEvent): ClientCoordinates {
+  // @ts-ignore
+  const { clientX, clientY, touches } = event
+
+  if (clientX && clientY) {
+    return {
+      clientX,
+      clientY,
+    }
+  }
+  return touches[0]
 }
