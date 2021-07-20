e693853cddedba18d12a9ca3e63fdd2a007f6eaf
Tang Zhi <tgioer@163.com>
2021-07-20 21:05:41 +0800

    Add the getBoundingClientRects function

--- packages/core/src/utils/rect.ts
+++ packages/core/src/utils/rect.ts
@@ -1,6 +1,6 @@
 import { createSelectorQuery } from "@tarojs/taro"
 import { inBrowser } from "./base"
-import { elementUnref, isWindow } from "./dom/element"
+import { createNodesRef, elementUnref, isWindow } from "./dom/element"
 
 export interface BoundingClientRect {
   dataset: Record<string, any>
@@ -13,7 +13,7 @@ export interface BoundingClientRect {
   height: number
 }
 
-function makeBoundingClientRect(width: number, height: number) {
+export function makeBoundingClientRect(width: number, height: number) {
   return {
     top: 0,
     left: 0,
@@ -37,12 +37,35 @@ export function getBoundingClientRect(elementOrRef: any): Promise<BoundingClient
       return Promise.resolve((element.getBoundingClientRect() as unknown) as BoundingClientRect)
     } else {
       return new Promise<BoundingClientRect>((resolve) => {
-        createSelectorQuery()
-          .select("#" + element.uid)
-          .boundingClientRect(resolve)
-          .exec()
+        createNodesRef(element).boundingClientRect(resolve).exec()
       })
     }
   }
   return Promise.resolve(makeBoundingClientRect(0, 0))
+}
+
+export function getBoundingClientRects(
+  elementOrRef: any,
+  selector: string,
+): Promise<BoundingClientRect[]> {
+  const element = elementUnref(elementOrRef)
+  if (element) {
+    if (inBrowser) {
+      const rects: BoundingClientRect[] = []
+      element
+        .querySelectorAll(selector)
+        .forEach((oneElement) =>
+          rects.push((oneElement.getBoundingClientRect() as unknown) as BoundingClientRect),
+        )
+      return Promise.resolve(rects)
+    } else {
+      return new Promise<BoundingClientRect[]>((resolve) => {
+        createSelectorQuery()
+          .selectAll("#" + element.uid + selector)
+          .boundingClientRect((rects) => resolve((rects as unknown) as BoundingClientRect[]))
+          .exec()
+      })
+    }
+  }
+  return Promise.resolve([])
 }
