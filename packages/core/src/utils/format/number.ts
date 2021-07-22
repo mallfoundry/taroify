b0b03e0ca3227b9446000cf2a4ed26595ea45598
Tang Zhi <tgioer@163.com>
2021-07-22 17:44:33 +0800

    Add some trimExtraChar, formatNumber, addNumber functions

--- packages/core/src/utils/format/number.ts
+++ packages/core/src/utils/format/number.ts
@@ -1,3 +1,43 @@
+import * as _ from "lodash"
+
 export function clamp(num: number, min: number, max: number): number {
   return Math.min(Math.max(num, min), max)
+}
+
+function trimExtraChar(value: string, char: string, regExp: RegExp) {
+  const index = value.indexOf(char)
+
+  if (index === -1) {
+    return value
+  }
+
+  if (char === "-" && index !== 0) {
+    return value.slice(0, index)
+  }
+
+  return value.slice(0, index + 1) + value.slice(index).replace(regExp, "")
+}
+
+export function formatNumber(value: string, allowDot = true, allowMinus = true) {
+  if (allowDot) {
+    value = trimExtraChar(value, ".", /\./g)
+  } else {
+    value = value.split(".")[0]
+  }
+
+  if (allowMinus) {
+    value = trimExtraChar(value, "-", /-/g)
+  } else {
+    value = value.replace(/-/, "")
+  }
+
+  const regExp = allowDot ? /[^-0-9.]/g : /[^-0-9]/g
+
+  return value.replace(regExp, "")
+}
+
+// add num and avoid float number
+export function addNumber(num1: number, num2: number) {
+  const cardinal = 10 ** 10
+  return Math.round((_.toNumber(num1) + _.toNumber(num2)) * cardinal) / cardinal
 }
