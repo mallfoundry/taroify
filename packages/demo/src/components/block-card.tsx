97ba7cb73fce6315ca33ada3706c32105fed50a8
Tang Zhi <tgioer@163.com>
2021-07-22 13:38:36 +0800

    Add a BlockCard component

---
+++ packages/demo/src/components/block-card.tsx
@@ -0,0 +1,21 @@
+import { View } from "@tarojs/components"
+import classNames from "classnames"
+import * as React from "react"
+import { ReactNode } from "react"
+import { demoPrefixClassname } from "../styles/prefix"
+import "./block-card.scss"
+
+interface BlockCardProps {
+  className?: string
+  children?: ReactNode
+}
+
+export default function BlockCard(props: BlockCardProps) {
+  const { className, children } = props
+  return (
+    <View
+      className={classNames(demoPrefixClassname("block-card"), className)}
+      children={children}
+    />
+  )
+}
