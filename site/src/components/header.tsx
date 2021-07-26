4d413dade2bed61f0c85ee39553b2b3f3386b064
Tang Zhi <tgioer@163.com>
2021-07-26 18:01:47 +0800

    Add github, gitee svg

--- site/src/components/header.tsx
+++ site/src/components/header.tsx
@@ -1,14 +1,29 @@
 import * as React from "react"
+
+import gitee from "./gitee.svg"
+import github from "./github.svg"
 import "./header.scss"
 
 export default function Handler() {
   return (
-    <header className="vant-header">
-      <div className="vant-header-top">
-        <div className="vant-header-logo">
+    <header className="van-header">
+      <div className="van-header__top">
+        <div className="van-header__logo">
           <img src="https://img01.yzcdn.cn/vant/logo.png" alt="taroify logo" />
           <span>Taroify</span>
         </div>
+        <ul className="van-header__top-nav">
+          <li className="van-header__top-nav-item">
+            <a href="https://gitee.com/mallfoundry/taroify/">
+              <img src={gitee} alt="" />
+            </a>
+          </li>
+          <li className="van-header__top-nav-item">
+            <a href="https://github.com/mallfoundry/taroify/">
+              <img src={github} alt="" />
+            </a>
+          </li>
+        </ul>
       </div>
     </header>
   )
