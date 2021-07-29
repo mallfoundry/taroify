75a0896ecf5f13a0cbced8bb733624efc501b7e4
Tang Zhi <tgioer@163.com>
2021-07-29 18:17:34 +0800

    Add some form files

---
+++ packages/core/src/form/form-control.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+
+interface FormControlProps {
+  children?: ReactNode
+}
+
+function FormControl(props: FormControlProps) {}
+
+export default FormControl
---
+++ packages/core/src/form/form-label.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+
+interface FormLabelProps {
+  children?: ReactNode
+}
+
+function FormLabel(props: FormLabelProps) {}
+
+export default FormLabel
