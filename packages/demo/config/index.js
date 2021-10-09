8866c294426bea1675cb422da65b5ea2e918f9bb
Tang Zhi <tgioer@163.com>
2021-10-09 12:50:15 +0800

    Update version to '0.0.13-alpha.0'

--- packages/demo/config/index.js
+++ packages/demo/config/index.js
@@ -64,7 +64,7 @@ const config = {
   },
 }
 
-module.exports = function(merge) {
+module.exports = function (merge) {
   if (process.env.NODE_ENV === "development") {
     return merge({}, config, require("./dev"))
   }
