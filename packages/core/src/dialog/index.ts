bf8572630e7819f16ffd75da56d1a71978f33efb
Tang Zhi <tgioer@163.com>
2021-10-19 19:18:27 +0800

    Refactor namespace to files

--- packages/core/src/dialog/index.ts
+++ packages/core/src/dialog/index.ts
@@ -1,1 +1,19 @@
-export { default } from "./dialog"
+import DialogComponent, { DialogProps } from "./dialog"
+import DialogActions from "./dialog-actions"
+import DialogContent from "./dialog-content"
+import DialogHeader from "./dialog-header"
+
+interface DialogInterface {
+  (props: DialogProps): JSX.Element
+
+  Header: typeof DialogHeader
+  Content: typeof DialogContent
+  Actions: typeof DialogActions
+}
+
+const Dialog = DialogComponent as DialogInterface
+Dialog.Header = DialogHeader
+Dialog.Content = DialogContent
+Dialog.Actions = DialogActions
+
+export default Dialog
