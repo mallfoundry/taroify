7ece545518c5feee54bf7a5a140a5634f36874c1
Tang Zhi <tgioer@163.com>
2021-10-24 19:16:36 +0800

    Add ConfigProvider on demand

--- packages/demo/src/pages/home/index.tsx
+++ packages/demo/src/pages/home/index.tsx
@@ -1,3 +1,4 @@
+import { ConfigProvider } from "@taroify/core"
 import { Image, Text, View } from "@tarojs/components"
 import * as _ from "lodash"
 import * as React from "react"
@@ -21,19 +22,21 @@ function renderNavs() {
 
 export default function Home() {
   return (
-    <View className={demoPrefixClassname("home")}>
-      <View className={demoPrefixClassname("home__title")}>
-        <Image
-          className={demoPrefixClassname("home__logo")}
-          mode="aspectFit"
-          src="https://img01.yzcdn.cn/vant/logo.png"
-        />
-        <Text className={demoPrefixClassname("home__name")}>Taroify</Text>
+    <ConfigProvider>
+      <View className={demoPrefixClassname("home")}>
+        <View className={demoPrefixClassname("home__title")}>
+          <Image
+            className={demoPrefixClassname("home__logo")}
+            mode="aspectFit"
+            src="https://img01.yzcdn.cn/vant/logo.png"
+          />
+          <Text className={demoPrefixClassname("home__name")}>Taroify</Text>
+        </View>
+        <View className={demoPrefixClassname("home__description")}>
+          轻量、可靠的小程序端 Taro 组件库
+        </View>
+        {renderNavs()}
       </View>
-      <View className={demoPrefixClassname("home__description")}>
-        轻量、可靠的小程序端 Taro 组件库
-      </View>
-      {renderNavs()}
-    </View>
+    </ConfigProvider>
   )
 }
