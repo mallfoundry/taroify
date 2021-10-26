850f1f484b6e7a7d43fcf96657fb0ca5c235fde1
Tang Zhi <tgioer@163.com>
2021-10-26 23:09:19 +0800

    Add the useRefs function

--- packages/core/src/utils/state.ts
+++ packages/core/src/utils/state.ts
@@ -1,4 +1,4 @@
-import { MutableRefObject, useEffect, useRef } from "react"
+import { createRef, MutableRefObject, useCallback, useEffect, useRef } from "react"
 
 export function usePrevious<T = any>(value: T): T {
   const previousRef = useRef<T>(value)
@@ -14,4 +14,20 @@ export function useToRef<T = any>(value: T): MutableRefObject<T> {
     stateRef.current = value
   }, [value])
   return stateRef
+}
+
+export function useRefs<T = Element>() {
+  const refs = useRef<MutableRefObject<T>[]>([])
+
+  const setIndexRefs = useCallback(
+    (index: number) => (el: unknown) => {
+      if (!refs.current[index]) {
+        refs.current[index] = createRef() as MutableRefObject<T>
+      }
+      refs.current[index].current = el as T
+    },
+    [],
+  )
+
+  return [refs.current, setIndexRefs] as const
 }
