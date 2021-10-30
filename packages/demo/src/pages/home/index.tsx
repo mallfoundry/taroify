3891428bfc8c47911db0f683432a40f88e8acba9
Tang Zhi <tgioer@163.com>
2021-10-30 11:37:25 +0800

    Remove @taroify/icons style

--- packages/demo/src/pages/home/index.tsx
+++ packages/demo/src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import { ConfigProvider } from "@taroify/core"
 import { Image, Text, View } from "@tarojs/components"
 import * as _ from "lodash"
 import * as React from "react"
@@ -22,21 +21,19 @@ function renderNavs() {
 
 export default function Home() {
   return (
-    <ConfigProvider>
-      <View className={demoPrefixClassname("home")}>
-        <View className={demoPrefixClassname("home__title")}>
-          <Image
-            className={demoPrefixClassname("home__logo")}
-            mode="aspectFit"
-            src="https://img01.yzcdn.cn/vant/logo.png"
-          />
-          <Text className={demoPrefixClassname("home__name")}>Taroify</Text>
-        </View>
-        <View className={demoPrefixClassname("home__description")}>
-          轻量、可靠的小程序端 Taro 组件库
-        </View>
-        {renderNavs()}
+    <View className={demoPrefixClassname("home")}>
+      <View className={demoPrefixClassname("home__title")}>
+        <Image
+          className={demoPrefixClassname("home__logo")}
+          mode="aspectFit"
+          src="https://img01.yzcdn.cn/vant/logo.png"
+        />
+        <Text className={demoPrefixClassname("home__name")}>Taroify</Text>
       </View>
-    </ConfigProvider>
+      <View className={demoPrefixClassname("home__description")}>
+        轻量、可靠的小程序端 Taro 组件库
+      </View>
+      {renderNavs()}
+    </View>
   )
 }
