36fe0b571ed940bc1cf05a24691d8cae2505885e
Tang Zhi <tgioer@163.com>
2021-11-26 18:51:54 +0800

    Add compareVersion

---
+++ packages/core/src/utils/version.ts
@@ -0,0 +1,56 @@
+import { canIUse, getSystemInfoSync } from "@tarojs/taro"
+import { inQQ, inWechat } from "./base"
+
+function compareVersion(o1: string, o2: string) {
+  const v1 = o1.split(".")
+  const v2 = o2.split(".")
+  const len = Math.max(v1.length, v2.length)
+
+  while (v1.length < len) {
+    v1.push("0")
+  }
+  while (v2.length < len) {
+    v2.push("0")
+  }
+
+  for (let i = 0; i < len; i++) {
+    const num1 = parseInt(v1[i], 10)
+    const num2 = parseInt(v2[i], 10)
+
+    if (num1 > num2) {
+      return 1
+    }
+    if (num1 < num2) {
+      return -1
+    }
+  }
+
+  return 0
+}
+
+function gte(version: string) {
+  const system = getSystemInfoSync()
+  return compareVersion(system.SDKVersion, version) >= 0
+}
+
+export function canIUseModel() {
+  return gte("2.9.3")
+}
+
+export function canIUseAnimate() {
+  return gte("2.9.0")
+}
+
+export function canIUseNextTick() {
+  return canIUse("nextTick")
+}
+
+export function canIUseCanvas2d() {
+  if (inQQ) {
+    return false
+  }
+  if (inWechat) {
+    return gte("2.9.0")
+  }
+  return true
+}
