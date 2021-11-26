9c5f08dcb5b04d7276f876869c5faae1e4a7ff71
Tang Zhi <tgioer@163.com>
2021-11-26 18:48:25 +0800

    Remove eslint-disable-next-line

--- packages/core/src/hooks/use-mounted.ts
+++ packages/core/src/hooks/use-mounted.ts
@@ -1,9 +1,5 @@
 import useRenderedEffect from "./use-rendered-effect"
 
 export default function useMounted(cb: (...args: any[]) => any) {
-  useRenderedEffect(
-    cb,
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  )
+  useRenderedEffect(cb, [])
 }
