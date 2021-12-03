445ae1d94c9e56343da8da66a2b76b5ce73da5fb
Tang Zhi <tgioer@163.com>
2021-12-03 09:12:15 +0800

    Use focus instead of focused

--- packages/demo/src/pages/form/password-input/index.tsx
+++ packages/demo/src/pages/form/password-input/index.tsx
@@ -1,46 +1,119 @@
-import { PasswordInput } from "@taroify/core"
+import { NumberKeyboard, PasswordInput } from "@taroify/core"
 import * as React from "react"
+import { useState } from "react"
 import Block from "../../../components/block"
 import Page from "../../../components/page"
 import "./index.scss"
 
-function BasicPasswordInput() {
-  return <PasswordInput focused value="123" />
+interface OpenKeyboardProps {
+  open?: string
+
+  onOpen?(open: string): void
 }
 
-function CustomLengthPasswordInput() {
-  return <PasswordInput length={6} focused value="123" />
+function BasicPasswordInput(props: OpenKeyboardProps) {
+  const { open, onOpen } = props
+  const [value, setValue] = useState("123")
+
+  return (
+    <>
+      <PasswordInput focus value={value} onFocus={() => onOpen?.("basic")} />
+      <NumberKeyboard
+        open={open === "basic"}
+        onKeyPress={(key) => setValue(value + key)}
+        onBackspace={() => setValue(value.substring(0, value.length - 1))}
+        onHide={() => onOpen?.("")}
+      />
+    </>
+  )
 }
 
-function GutterPasswordInput() {
-  return <PasswordInput gutter={10} focused value="123" />
+function CustomLengthPasswordInput(props: OpenKeyboardProps) {
+  const { open, onOpen } = props
+  const [value, setValue] = useState("123")
+
+  return (
+    <>
+      <PasswordInput focus length={6} value={value} onFocus={() => onOpen?.("length")} />
+      <NumberKeyboard
+        open={open === "length"}
+        onKeyPress={(key) => setValue(value + key)}
+        onBackspace={() => setValue(value.substring(0, value.length - 1))}
+        onHide={() => onOpen?.("")}
+      />
+    </>
+  )
 }
 
-function NoMaskPasswordInput() {
-  return <PasswordInput mask={false} focused value="123" />
+function GutterPasswordInput(props: OpenKeyboardProps) {
+  const { open, onOpen } = props
+  const [value, setValue] = useState("123")
+
+  return (
+    <>
+      <PasswordInput focus gutter={10} value={value} onFocus={() => onOpen?.("gutter")} />
+      <NumberKeyboard
+        open={open === "gutter"}
+        onKeyPress={(key) => setValue(value + key)}
+        onBackspace={() => setValue(value.substring(0, value.length - 1))}
+        onHide={() => onOpen?.("")}
+      />
+    </>
+  )
 }
 
-function PasswordInputWithInfo() {
-  return <PasswordInput info="密码为 6 位数字" focused value="123" />
+function NoMaskPasswordInput(props: OpenKeyboardProps) {
+  const { open, onOpen } = props
+  const [value, setValue] = useState("123")
+
+  return (
+    <>
+      <PasswordInput focus mask={false} value={value} onFocus={() => onOpen?.("mask")} />
+      <NumberKeyboard
+        open={open === "mask"}
+        onKeyPress={(key) => setValue(value + key)}
+        onBackspace={() => setValue(value.substring(0, value.length - 1))}
+        onHide={() => onOpen?.("")}
+      />
+    </>
+  )
+}
+
+function PasswordInputWithInfo(props: OpenKeyboardProps) {
+  const { open, onOpen } = props
+  const [value, setValue] = useState("123")
+
+  return (
+    <>
+      <PasswordInput focus info="密码为 6 位数字" value={value} onFocus={() => onOpen?.("info")} />
+      <NumberKeyboard
+        open={open === "info"}
+        onKeyPress={(key) => setValue(value + key)}
+        onBackspace={() => setValue(value.substring(0, value.length - 1))}
+        onHide={() => onOpen?.("")}
+      />
+    </>
+  )
 }
 
 export default function PasswordInputDemo() {
+  const [open, setOpen] = useState("")
   return (
     <Page title="PasswordInput 密码输入框" className="password-input-demo">
       <Block title="基础用法">
-        <BasicPasswordInput />
+        <BasicPasswordInput open={open} onOpen={setOpen} />
       </Block>
       <Block title="自定义长度">
-        <CustomLengthPasswordInput />
+        <CustomLengthPasswordInput open={open} onOpen={setOpen} />
       </Block>
       <Block title="格子间距">
-        <GutterPasswordInput />
+        <GutterPasswordInput open={open} onOpen={setOpen} />
       </Block>
       <Block title="明文展示">
-        <NoMaskPasswordInput />
+        <NoMaskPasswordInput open={open} onOpen={setOpen} />
       </Block>
       <Block title="明文展示">
-        <PasswordInputWithInfo />
+        <PasswordInputWithInfo open={open} onOpen={setOpen} />
       </Block>
     </Page>
   )
