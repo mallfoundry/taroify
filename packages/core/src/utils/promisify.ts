41427a341154c996ff50e2888560d034775d1916
Tang Zhi <tgioer@163.com>
2021-12-14 20:50:32 +0800

    Add fulfillPromise & isPromise

---
+++ packages/core/src/utils/promisify.ts
@@ -0,0 +1,11 @@
+import * as _ from "lodash"
+
+export function isPromise<T = any>(val: any): val is Promise<T> {
+  return _.isObjectLike(val) && _.isFunction((val as Promise<T>).then) && _.isFunction(val.catch)
+}
+
+export function fulfillPromise(promise: PromiseLike<any>) {
+  if (isPromise(promise)) {
+    promise?.then(() => {})
+  }
+}
