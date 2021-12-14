60c967134cefdd4f1f2b12a7d60e3883d01afda3
Tang Zhi <tgioer@163.com>
2021-12-14 21:02:16 +0800

    Add defaultOpen & open props (#186)
    * Add fulfillPromise & isPromise
    
    * Add defaultOpen & open props
    
    * Update demo
    
    * Update docs

--- packages/demo/src/pages/action/swipe-cell/index.tsx
+++ packages/demo/src/pages/action/swipe-cell/index.tsx
@@ -7,57 +7,57 @@ import "./index.scss"
 
 function BasicSwipeCell() {
   return (
-    <Block title="基础用法">
-      <SwipeCell>
-        <SwipeCell.Actions side="left">
-          <Button variant="contained" shape="square" color="primary">
-            选择
-          </Button>
-        </SwipeCell.Actions>
-        <Cell bordered={false} title="单元格">
-          内容
-        </Cell>
-        <SwipeCell.Actions side="right">
-          <Button variant="contained" shape="square" color="danger">
-            删除
-          </Button>
-          <Button variant="contained" shape="square" color="primary">
-            收藏
-          </Button>
-        </SwipeCell.Actions>
-      </SwipeCell>
-    </Block>
+    <SwipeCell>
+      <SwipeCell.Actions side="left">
+        <Button variant="contained" shape="square" color="primary">
+          选择
+        </Button>
+      </SwipeCell.Actions>
+      <Cell bordered={false} title="单元格">
+        内容
+      </Cell>
+      <SwipeCell.Actions side="right">
+        <Button variant="contained" shape="square" color="danger">
+          删除
+        </Button>
+        <Button variant="contained" shape="square" color="primary">
+          收藏
+        </Button>
+      </SwipeCell.Actions>
+    </SwipeCell>
   )
 }
 
 function SwipeCellWithCustomContent() {
   return (
-    <Block title="自定义内容">
-      <SwipeCell className="custom-swipe-cell">
-        <View className="custom-card">
-          <Image className="custom-card__thumb" src="https://img01.yzcdn.cn/vant/ipad.jpeg" />
-          <View className="custom-card__content">
-            <View className="custom-card__title">商品标题</View>
-          </View>
+    <SwipeCell className="custom-swipe-cell">
+      <View className="custom-card">
+        <Image className="custom-card__thumb" src="https://img01.yzcdn.cn/vant/ipad.jpeg" />
+        <View className="custom-card__content">
+          <View className="custom-card__title">商品标题</View>
         </View>
-        <SwipeCell.Actions side="right">
-          <Button variant="contained" shape="square" color="danger">
-            删除
-          </Button>
-          <Button variant="contained" shape="square" color="primary">
-            收藏
-          </Button>
-        </SwipeCell.Actions>
-      </SwipeCell>
-    </Block>
+      </View>
+      <SwipeCell.Actions side="right">
+        <Button variant="contained" shape="square" color="danger">
+          删除
+        </Button>
+        <Button variant="contained" shape="square" color="primary">
+          收藏
+        </Button>
+      </SwipeCell.Actions>
+    </SwipeCell>
   )
 }
 
 export default function SwipeCellDemo() {
   return (
     <Page title="SwipeCell 滑动单元格" className="swipe-cell-demo">
-      <BasicSwipeCell />
-      <SwipeCellWithCustomContent />
+      <Block title="基础用法">
+        <BasicSwipeCell />
+      </Block>
+      <Block title="自定义内容">
+        <SwipeCellWithCustomContent />
+      </Block>
     </Page>
   )
 }
