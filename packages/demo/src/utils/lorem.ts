2a5c072b1602a0b0047d6a9c2d370d2bc978ec0b
Tang Zhi <tgioer@163.com>
2021-12-15 20:01:35 +0800

    Add SafeArea component (#195)

---
+++ packages/demo/src/utils/lorem.ts
@@ -0,0 +1,14 @@
+import { LoremIpsum } from "lorem-ipsum"
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+})
+
+export default lorem
