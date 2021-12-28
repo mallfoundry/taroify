412838c57b0be2fa6dccecbaddb0f718fa04fa5e
Tang Zhi <tgioer@163.com>
2021-12-28 16:47:31 +0800

    Rename FieldInstance to FormItemInstance

--- packages/core/src/form/use-form-field.ts
+++ packages/core/src/form/use-form-field.ts
@@ -1,9 +1,9 @@
 import { useContext, useEffect } from "react"
-import { FieldInstance } from "../field"
 import FormContext from "./form.context"
+import { FormItemInstance } from "./form.shared"
 import useForm from "./use-form"
 
-export default function useFormField(name?: string, field?: FieldInstance) {
+export default function useFormField(name?: string, field?: FormItemInstance) {
   const { name: formName } = useContext(FormContext)
   const form = useForm(formName)
   if (name && field) {
