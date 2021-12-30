0224b15ee09d0ed100de7082398a0cc271c3bc0e
Tang Zhi <tgioer@163.com>
2021-12-30 16:30:10 +0800

    Exports FormControlHandler type

--- packages/core/src/form/control/index.ts
+++ packages/core/src/form/control/index.ts
@@ -1,3 +1,4 @@
 import "./form-control-handlers"
 
 export { registerFormControlHandler, doFormControlHandler } from "./form-control-handler"
+export type { default as FormControlHandler } from "./form-control-handler"
