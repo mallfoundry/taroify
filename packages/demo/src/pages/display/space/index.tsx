b13ba265721de7be0d5bc165c2d9fd15fde117f6
coderYarn <642997741@qq.com>
2022-01-02 13:39:52 +0800

    ✨ feat(demo/pages/display):add space components

---
+++ packages/demo/src/pages/display/space/index.tsx
@@ -0,0 +1,76 @@
+import { Button, Space } from "@taroify/core"
+import * as React from "react"
+import Block from "../../../components/block"
+import Page from "../../../components/page"
+import "./index.scss"
+
+export default function SpaceDemo() {
+  return (
+    <Page title="Space 间隔" className="space-demo">
+      <Block title="基础用法">
+        <Space>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+      <Block title="垂直">
+        <Space direction="vertical">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+      <Block title="间隙">
+        <Space size="small">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+        <Space size="medium">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+        <Space size="large">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+      <Block title="靠右">
+        <Space justify="space-around">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+      <Block title="居中">
+        <Space justify="center">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+      <Block title="环绕">
+        <Space justify="space-around">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+      <Block title="两端对齐">
+        <Space justify="space-between">
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+      <Block title="不换行">
+        <Space wrap>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+          <Button color="primary">主要按钮</Button>
+        </Space>
+      </Block>
+    </Page>
+  )
+}
