c0861a53a0739f45fa51d66db363c3c58d22ed3e
coderYarn <642997741@qq.com>
2022-01-11 17:29:35 +0800

    ✨ feat(core/src/avatars):add  the avatars component

--- packages/core/src/avatars/avatars-group.tsx
+++ packages/core/src/avatars/avatars-group.tsx
@@ -1,15 +1,55 @@
+import { View } from "@tarojs/components"
 import * as React from "react"
-import Avatars from "./avatars"
+import Avatars, { variantEnum, VarinatType } from "./avatars"
 
 interface AvatarsGroupProps {
-  children: React.ReactNode
+  children: React.ReactNode[]
   max?: number
+  variant?: variantEnum | VarinatType
+  spacing?: number
+  total?: number
 }
-export default function AvatarsGroup({ max, children }: AvatarsGroupProps): JSX.Element {
+export default function AvatarsGroup({
+  variant = variantEnum.CIRCULAR,
+  max = 0,
+  children,
+  spacing = 6,
+  total,
+}: AvatarsGroupProps): JSX.Element {
+  const length = React.useMemo(() => {
+    return children.length - max
+  }, [children, max])
+  const NewReactNode: React.ReactNode[] = []
+
+  React.Children.forEach(children, (child, index) => {
+    if (React.isValidElement(child)) {
+      if (NewReactNode.length < max || total) {
+        NewReactNode.push(
+          React.cloneElement(
+            child,
+            {
+              key: index,
+              position: "position",
+              left: child.props.width - spacing || index * (36 - spacing),
+              ZIndex: 999 - index,
+              variant,
+            },
+            null,
+          ),
+        )
+      }
+    }
+  })
+
   return (
     <>
-      {children}
-      <Avatars>+2</Avatars>
+      <View style={{ position: "relative" }}>
+        {NewReactNode}
+        <Avatars variant={variant} position={"position"} left={NewReactNode.length * (36 - spacing)}>
+          +{total ? total - children.length : length}
+        </Avatars>
+      </View>
+      <Avatars sx={{background:"transparent"}}></Avatars>
     </>
   )
 }
--- packages/core/src/avatars/avatars.tsx
+++ packages/core/src/avatars/avatars.tsx
@@ -5,22 +5,25 @@ import { Image } from "@taroify/core"
 import { prefixClassname } from "../styles"
 
 interface AvatarsSx {
-  height?: string
-  width?: string
+  height?: number
+  width?: number
   background?: string
 }
-enum variantEnum {
+export enum variantEnum {
   SQUARE = "square",
   ROUNDED = "rounded",
   CIRCULAR = "circular",
 }
-type VarinatType = "square" | "rounded" | "circular"
+export type VarinatType = "square" | "rounded" | "circular"
 export interface AvatarsProps {
   children?: any
   sx?: AvatarsSx
   src?: string
   alt?: string
   variant?: variantEnum | VarinatType
+  position?: string
+  left?:any
+  ZIndex?:number
 }
 
 function Avatars({
@@ -29,16 +32,27 @@ function Avatars({
   src,
   alt,
   variant = variantEnum.CIRCULAR,
+  position,
+  left,
+  ZIndex
 }: AvatarsProps): JSX.Element {
-  const { width = "36px", height = "36px", background = "gray" } = sx
+  const { width = 36, height = 36, background = "gray" } = sx
   return (
     <View
       className={classNames(prefixClassname("avatars"), {
         [prefixClassname("avatars--circular")]: variant === "circular",
         [prefixClassname("avatars--square")]: variant === "square",
         [prefixClassname("avatars--rounded")]: variant === "rounded",
+        [prefixClassname("avatars--position")]: position === "position",
       })}
-      style={{ background: background, width: width, height: height }}
+      style={{
+        background: background,
+        width: width + "px",
+        height: height + "px",
+        lineHeight: height + "px",
+        left: left+"px",
+        zIndex:ZIndex
+      }}
     >
       {src ? <Image alt={alt} src={src}></Image> : children}
     </View>
