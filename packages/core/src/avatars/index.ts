c8247a1db08daeeb68f212156788a3e8d61cf13f
coderYarn <642997741@qq.com>
2022-01-11 15:59:19 +0800

    feat(core/src/avatars):exprot avatarsGroup compoents

--- packages/core/src/avatars/index.ts
+++ packages/core/src/avatars/index.ts
@@ -1,2 +1,13 @@
-export { default } from "./avatars"
+import AvatarsElement, { AvatarsProps } from "./avatars"
+import AvatarsGroup from "./avatars-group"
 
+interface AvatarsInterface {
+  (props: AvatarsProps): JSX.Element
+
+  Group: typeof AvatarsGroup
+}
+
+const Avatars = AvatarsElement as AvatarsInterface
+Avatars.Group = AvatarsGroup
+
+export default Avatars
