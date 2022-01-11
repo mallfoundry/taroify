3c9fb55fe60f11f3d2305693da4dbe9169ec2594
Tang Zhi <tgioer@163.com>
2022-01-11 20:23:33 +0800

    Add getPickerOptionKey function

--- packages/core/src/picker/picker.shared.ts
+++ packages/core/src/picker/picker.shared.ts
@@ -1,13 +1,15 @@
+import * as _ from "lodash"
 import { Key, ReactNode } from "react"
+import { isTextElement } from "../utils/validate"
 
 export const DEFAULT_SIBLING_COUNT = 3
 
-export type PickerMode = "flat" | "cascade"
-
 export interface PickerOptionObject {
+  key?: Key
   index?: number
   value?: any
   disabled?: boolean
+  label?: ReactNode
   children?: ReactNode
 
   // for custom filed names
@@ -21,4 +23,13 @@ export interface PickerColumnObject {
 
   // for custom filed names
   [key: string]: any
+}
+
+export function getPickerOptionKey(option: PickerOptionObject) {
+  const { key, value, label, children } = option
+  const newKey = key ?? value ?? label
+  if (_.isUndefined(newKey) && isTextElement(children)) {
+    return children
+  }
+  return newKey
 }
