cca15c38cf3b734477da5c8540090b505c5eac86
coderYarn <642997741@qq.com>
2022-01-11 15:58:20 +0800

    ✨ feat(core/src/avatars):improve the component

--- packages/core/src/avatars/avatars.tsx
+++ packages/core/src/avatars/avatars.tsx
@@ -25,11 +25,12 @@ export interface AvatarsProps {
 
 function Avatars({
   children,
-  sx = { width: "36px", height: "36px", background: "gray" },
+  sx = {},
   src,
   alt,
   variant = variantEnum.CIRCULAR,
 }: AvatarsProps): JSX.Element {
+  const { width = "36px", height = "36px", background = "gray" } = sx
   return (
     <View
       className={classNames(prefixClassname("avatars"), {
@@ -37,10 +38,9 @@ function Avatars({
         [prefixClassname("avatars--square")]: variant === "square",
         [prefixClassname("avatars--rounded")]: variant === "rounded",
       })}
-      style={sx}
+      style={{ background: background, width: width, height: height }}
     >
-      {children}
-      <Image alt={alt} src={src}></Image>
+      {src ? <Image alt={alt} src={src}></Image> : children}
     </View>
   )
 }
