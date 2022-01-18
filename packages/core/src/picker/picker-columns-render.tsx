84efdfab4400d8da59d7c98a8162ae1c8109a452
Tang Zhi <tgioer@163.com>
2022-01-18 13:40:58 +0800

    Adopt layered design (#269)
    * Adopt layered design
    
    * Exports { PickerProps, PickerColumnsProps, PickerContext} types

---
+++ packages/core/src/picker/picker-columns-render.tsx
@@ -0,0 +1,112 @@
+import { View } from "@tarojs/components"
+import { ViewProps } from "@tarojs/components/types/View"
+import classNames from "classnames"
+import * as _ from "lodash"
+import * as React from "react"
+import { CSSProperties, ReactNode, useMemo } from "react"
+import { prefixClassname } from "../styles"
+import { HAIRLINE_BORDER_UNSET_TOP_BOTTOM } from "../styles/hairline"
+import { preventDefault } from "../utils/dom/event"
+import { addUnitPx } from "../utils/format/unit"
+import { useRendered } from "../utils/state"
+import PickerColumn from "./picker-column"
+import { getPickerOptionKey, PickerOptionObject } from "./picker.shared"
+import usePickerOptions from "./use-picker-options"
+
+export interface PickerColumnsRenderProps extends ViewProps {
+  style?: CSSProperties
+  children?: ReactNode
+
+  values?: any[]
+  readonly?: boolean
+  siblingCount: number
+
+  onChange?(option: PickerOptionObject, column: PickerOptionObject, emitChange?: boolean): void
+}
+
+function PickerColumnsRender(props: PickerColumnsRenderProps) {
+  const {
+    className,
+    style,
+    children,
+    readonly,
+    values,
+    siblingCount,
+    onChange,
+    ...restProps
+  } = props
+  const columns = usePickerOptions(children)
+
+  const visibleCount = siblingCount * 2
+
+  const itemHeight = 44
+
+  const wrapHeight = useMemo(() => itemHeight * visibleCount, [visibleCount])
+
+  const rootStyle = useMemo(
+    () => ({
+      ...style,
+      height: addUnitPx(wrapHeight),
+    }),
+    [style, wrapHeight],
+  )
+
+  const maskStyle = useMemo<CSSProperties>(
+    () => ({
+      backgroundSize: `100% ${addUnitPx((wrapHeight - itemHeight) / 2)}`,
+    }),
+    [wrapHeight],
+  )
+
+  const frameStyle = useMemo<CSSProperties>(
+    () => ({
+      height: addUnitPx(itemHeight),
+    }),
+    [],
+  )
+
+  const columnsRender = useRendered(() =>
+    _.map(columns, (column, columnIndex) => {
+      const { children: options, ...restColumnProps } = column
+      return (
+        <PickerColumn
+          key={getPickerOptionKey(column) ?? columnIndex}
+          // @ts-ignore
+          children={options}
+          readonly={readonly}
+          {...restColumnProps}
+          value={_.get(values, columnIndex)}
+          onChange={(option, emitChange) =>
+            onChange?.(
+              option,
+              {
+                ...column,
+                index: columnIndex,
+              },
+              emitChange,
+            )
+          }
+        />
+      )
+    }),
+  )
+
+  return (
+    <View
+      className={classNames(prefixClassname("picker__columns"), className)}
+      style={rootStyle}
+      catchMove
+      onTouchMove={preventDefault}
+      {...restProps}
+    >
+      {columnsRender}
+      <View className={prefixClassname("picker__mask")} style={maskStyle} />
+      <View
+        className={classNames([HAIRLINE_BORDER_UNSET_TOP_BOTTOM, prefixClassname("picker__frame")])}
+        style={frameStyle}
+      />
+    </View>
+  )
+}
+
+export default PickerColumnsRender
