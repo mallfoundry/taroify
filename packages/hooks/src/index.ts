23f65d0e009463175cfc04de125277cd1f65f583
Tang Zhi <tgioer@163.com>
2022-01-18 20:44:12 +0800

    Exports useForceUpdate, useToRef, useUncontrolled hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,2 +1,5 @@
-export { default as useCascader } from "./use-cascader"
 export { default as useArea } from "./use-area"
+export { default as useCascader } from "./use-cascader"
+export { default as useForceUpdate } from "./use-force-update"
+export { default as useToRef } from "./use-to-ref"
+export { default as useUncontrolled } from "./use-uncontrolled"
