a979788a2641f9a01f31cd5a78de2fa420e04876
Tang Zhi <tgioer@163.com>
2022-01-21 20:35:59 +0800

    Add onReset support

--- packages/core/src/form/use-form-field-value-effect.ts
+++ packages/core/src/form/use-form-field-value-effect.ts
@@ -1,15 +1,26 @@
-import { DependencyList, EffectCallback, useEffect, useRef } from "react"
+import { DependencyList, EffectCallback, useCallback, useContext, useEffect, useRef } from "react"
+import FormContext from "./form.context"
+import useForm from "./use-form"
 
 function useFormFieldValueEffect(effect: EffectCallback, deps?: DependencyList) {
-  const counterRef = useRef(0)
+  const { name: formName } = useContext(FormContext)
+  const form = useForm(formName)
+
+  const mountedRef = useRef(false)
+
+  const onReset = useCallback(() => (mountedRef.current = false), [])
+
+  useEffect(() => {
+    form?.addEventListener("reset", onReset)
+    return () => form?.removeEventListener("reset", onReset)
+  }, [form, onReset])
 
   useEffect(
     () => {
-      if (counterRef.current !== 0) {
+      if (mountedRef.current) {
         effect?.()
-      }
-      if (counterRef.current === 0) {
-        counterRef.current++
+      } else {
+        mountedRef.current = true
       }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
