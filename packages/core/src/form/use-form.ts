3846dbe8cd6f73af76079b1c473b007bd7d5b716
Tang Zhi <tgioer@163.com>
2022-01-21 20:36:39 +0800

    Add reset and errors

--- packages/core/src/form/use-form.ts
+++ packages/core/src/form/use-form.ts
@@ -1,8 +1,11 @@
 import { Events } from "@tarojs/taro"
 import * as _ from "lodash"
 import { useEffect, useMemo } from "react"
-import { FormItemInstance } from "./form.shared"
+import { getLogger } from "../utils/logger"
+import { FormItemInstance, FormValidError } from "./form.shared"
 import { validateAll } from "./form.validate"
+
+const { warn } = getLogger("useForm")
 
 function toMapValueArray<T>(map: Map<string, T>): T[] {
   const array: T[] = []
@@ -19,58 +22,37 @@ function getAttributiveForm(formName: string) {
 
 class FormAttributes {
   #fields = new Map<string, FormItemInstance>()
-  #fieldsValue: any = {}
+  #defaultValues: any = {}
+  #values: any = {}
+  #errors: Record<string, FormValidError> = {}
   #events = new Events()
 
   get fields() {
     return this.#fields
   }
 
-  get fieldsValue() {
-    return this.#fieldsValue
+  get defaultValues() {
+    return this.#defaultValues
   }
 
-  set fieldsValue(value: any) {
-    this.#fieldsValue = value
+  set defaultValues(defaultValues: any) {
+    this.#defaultValues = defaultValues
   }
 
-  getFieldsValue<V>(nameNames?: string | string[]) {
-    const names = _.isUndefined(nameNames) ? [] : _.isArray(nameNames) ? nameNames : [nameNames]
-
-    const values: any = {}
-
-    if (_.isEmpty(names)) {
-      for (const field of this.fields.values()) {
-        if (field.name) {
-          values[field.name] = _.get(this.fieldsValue, field.name)
-        }
-      }
-      return values
-    }
-
-    for (let attr of names) {
-      _.set(values, attr, _.get(this.fieldsValue, attr))
-    }
-
-    return values as V
+  get values() {
+    return this.#values
   }
 
-  findFields(predicate: (field: FormItemInstance) => boolean) {
-    const fields = toMapValueArray(this.fields)
-    return _.filter<FormItemInstance>(fields, predicate) as FormItemInstance[]
+  set values(value: any) {
+    this.#values = value
   }
 
-  addField(name: string, field: FormItemInstance) {
-    this.#fields.set(name, field)
+  get errors() {
+    return this.#errors
   }
 
-  hasField(name: string) {
-    return this.#fields.has(name)
-  }
-
-  removeField(name: string) {
-    this.#fields.delete(name)
-    _.unset(this.#fieldsValue, name)
+  set errors(errors: Record<string, FormValidError>) {
+    this.#errors = errors
   }
 
   addEventListener(event: string, listener: (...args: any[]) => void) {
@@ -85,9 +67,129 @@ class FormAttributes {
     this.#events.trigger(event, ...args)
   }
 
+  findFields(predicate: (field: FormItemInstance) => boolean) {
+    const fields = toMapValueArray(this.fields)
+    return _.filter<FormItemInstance>(fields, predicate) as FormItemInstance[]
+  }
+
+  addField(name: string, field: FormItemInstance) {
+    this.#fields.set(name, field)
+  }
+
+  removeField(name: string) {
+    this.#fields.delete(name)
+    _.unset(this.#values, name)
+  }
+
+  hasField(name: string) {
+    return this.#fields.has(name)
+  }
+
+  getErrors(nameNames?: string | string[]): Record<string, FormValidError> {
+    const names = _.isUndefined(nameNames) ? [] : _.isArray(nameNames) ? nameNames : [nameNames]
+    const { errors } = this
+
+    if (_.isEmpty(names)) {
+      return errors
+    }
+
+    return _.reduce<string, Record<string, FormValidError>>(
+      names,
+      (nameErrors, name) => {
+        const error = _.get(errors, name)
+        if (error) {
+          nameErrors[name] = error
+        }
+        return nameErrors
+      },
+      {},
+    )
+  }
+
+  setErrors(newErrors: FormValidError[], emitChange: boolean = true) {
+    const { errors } = this
+    _.forEach(newErrors, (error) => {
+      const { name } = error
+      if (name) {
+        const oldError = _.get(errors, name)
+        if (!_.isEqual(oldError?.errors, error?.errors)) {
+          _.set(errors, name, error)
+          if (emitChange) {
+            this.emitEvent(`fields.${name}.error.change`, errors)
+          }
+        }
+      }
+    })
+  }
+
+  resetErrors(errors: Record<string, FormValidError> = {}) {
+    this.errors = errors
+  }
+
+  getValues<V>(nameNames?: string | string[]) {
+    const names = _.isUndefined(nameNames) ? [] : _.isArray(nameNames) ? nameNames : [nameNames]
+
+    const values: any = {}
+
+    if (_.isEmpty(names)) {
+      for (const field of this.fields.values()) {
+        if (field.name) {
+          values[field.name] = _.get(this.values, field.name)
+        }
+      }
+      return values
+    }
+
+    for (let attr of names) {
+      _.set(values, attr, _.get(this.values, attr))
+    }
+
+    return values as V
+  }
+
+  setValues(newValues: any, emitChange: boolean = true) {
+    let changed = false
+    const { values } = this
+    _.forEach(newValues, (value, name) => {
+      const oldValue = _.get(values, name)
+      if (oldValue !== value) {
+        _.set(values, name, value)
+        if (emitChange) {
+          this.emitEvent(`fields.${name}.value.change`, value)
+          changed = true
+        }
+      }
+    })
+    if (changed) {
+      this.emitEvent("change", newValues, values)
+    }
+  }
+
+  resetValues(values: any = {}) {
+    this.values = values
+  }
+
   release() {
     this.#events?.off()
   }
+}
+
+function validateForm<V>(formName: string, name?: string | string[]) {
+  return new Promise<V>((resolve, reject) => {
+    const form = getAttributiveForm(formName)
+    Promise.resolve<string[]>(_.isUndefined(name) ? [] : _.isArray(name) ? name : [name])
+      .then((names: string[]) =>
+        form?.findFields((field) => _.isEmpty(names) || _.includes(names, field.name)),
+      )
+      .then(validateAll)
+      .then<V>(() => form?.getValues(name) as V)
+      .then(resolve)
+      .catch((errors: FormValidError[]) => {
+        form.resetErrors()
+        form.setErrors(errors)
+        reject(errors)
+      })
+  })
 }
 
 interface Form {
@@ -105,17 +207,42 @@ interface Form {
 
   findFields(predicate: (field: FormItemInstance) => boolean): FormItemInstance[]
 
-  validateFields<V>(name?: string | string[]): Promise<V>
+  setErrors(errors: FormValidError[]): void
 
-  setFieldsDefaultValue(values: any): void
+  resetErrors(): void
 
+  getErrors(name?: string | string[]): FormValidError[]
+
+  setDefaultValues(values: any): void
+
+  setValues(values: any): void
+
+  resetValues(values: any): void
+
+  getValues<V>(name?: string | string[]): V
+
+  validate<V>(name?: string | string[]): Promise<V>
+
+  reset(): void
+
+  /**
+   * @deprecated
+   */
   setFieldsValue(values: any): void
 
+  /**
+   * @deprecated
+   */
   getFieldsValue<V>(name?: string | string[]): V
+
+  /**
+   * @deprecated
+   */
+  validateFields<V>(name?: string | string[]): Promise<V>
 }
 
 function defineForm(formName: string) {
-  return class DelegatingForm {
+  return class DelegatingForm implements Form {
     get name() {
       return formName
     }
@@ -126,37 +253,6 @@ function defineForm(formName: string) {
 
     removeEventListener(event: string, listener: (...args: any[]) => void) {
       getAttributiveForm(formName)?.removeEventListener(event, listener)
-    }
-
-    setFieldsDefaultValue(newValues: any) {
-      const form = getAttributiveForm(formName)
-      if (form) {
-        const { fieldsValue } = form
-        _.forEach(newValues, (value, name) => _.set(fieldsValue, name, value))
-      }
-    }
-
-    setFieldsValue(newValues: any) {
-      const form = getAttributiveForm(formName)
-      if (form) {
-        let changed = false
-        const { fieldsValue } = form
-        _.forEach(newValues, (value, name) => {
-          const oldValue = _.get(fieldsValue, name)
-          if (oldValue !== value) {
-            changed = true
-            _.set(fieldsValue, name, value)
-            form.emitEvent(`fields.${name}.value.change`, value)
-          }
-        })
-        if (changed) {
-          form.emitEvent("change", newValues, fieldsValue)
-        }
-      }
-    }
-
-    getFieldsValue<V>(name?: string | string[]) {
-      return getAttributiveForm(formName)?.getFieldsValue<V>(name)
     }
 
     linkField(name?: string, field?: FormItemInstance) {
@@ -183,15 +279,70 @@ function defineForm(formName: string) {
       return getAttributiveForm(formName)?.findFields(predicate)
     }
 
+    setErrors(errors: FormValidError[]) {
+      getAttributiveForm(formName)?.setErrors(errors)
+    }
+
+    resetErrors() {
+      getAttributiveForm(formName)?.resetErrors()
+    }
+
+    getErrors(name?: string | string[]): FormValidError[] {
+      return _.reduce<FormValidError, FormValidError[]>(
+        getAttributiveForm(formName).getErrors(name) as any,
+        (errors, error) => {
+          errors.push(error)
+          return errors
+        },
+        [],
+      )
+    }
+
+    setDefaultValues(defaultValues: any): void {
+      const form = getAttributiveForm(formName)
+      if (form) {
+        form.defaultValues = defaultValues
+        form?.setValues(defaultValues)
+      }
+    }
+
+    setValues(newValues: any) {
+      getAttributiveForm(formName)?.setValues(newValues)
+    }
+
+    resetValues(values: any) {}
+
+    getValues<V>(name?: string | string[]): V {
+      return getAttributiveForm(formName)?.getValues<V>(name)
+    }
+
+    validate<V>(name?: string | string[]): Promise<V> {
+      return validateForm<V>(formName, name)
+    }
+
+    reset() {
+      const form = getAttributiveForm(formName)
+      const oldValues = _.cloneDeep(form.values)
+      const newValues = _.cloneDeep(form.defaultValues)
+      form?.resetValues(newValues)
+      form?.resetErrors()
+      form.emitEvent("change", newValues, oldValues)
+      form.emitEvent("reset")
+    }
+
+    setFieldsValue(newValues: any, emitChange?: boolean) {
+      warn("Please use setValues instead of setFieldsValue")
+      getAttributiveForm(formName)?.setValues(newValues)
+    }
+
+    getFieldsValue<V>(name?: string | string[]) {
+      warn("Please use getValues instead of getFieldsValue")
+      return getAttributiveForm(formName)?.getValues<V>(name)
+    }
+
     validateFields<V>(name?: string | string[]) {
-      return Promise.resolve<string[]>(_.isUndefined(name) ? [] : _.isArray(name) ? name : [name])
-        .then((names: string[]) =>
-          getAttributiveForm(formName)?.findFields(
-            (field) => _.isEmpty(names) || _.includes(names, field.name),
-          ),
-        )
-        .then(validateAll)
-        .then<V>(() => getAttributiveForm(formName)?.getFieldsValue(name) as V)
+      warn("Please use validate instead of validateFields")
+      return validateForm<V>(formName, name)
     }
   }
 }
@@ -252,25 +403,34 @@ interface UseFormOptions<V> {
   values?: V
 }
 
-export default function useForm<V = any>(name: string, options: UseFormOptions<V> = {}) {
+export default function useForm<V = any>(name: string, options: UseFormOptions<V> = {}): Form {
   const { defaultValues, values } = options
-  if (name && !formContainer.hasForm(name)) {
+  const hasForm = formContainer.hasForm(name)
+  if (!hasForm) {
     formContainer.newForm(name)
   }
 
-  useEffect(() => () => formContainer.releaseForm(name), [name])
-
   useEffect(
-    () => formContainer.getForm(name)?.setFieldsValue(defaultValues),
+    () => {
+      if (!hasForm && _.isPlainObject(defaultValues)) {
+        formContainer.getForm(name)?.setDefaultValues(defaultValues)
+      }
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [],
   )
 
   useEffect(
-    () => formContainer.getForm(name)?.setFieldsValue(values),
+    () => {
+      if (!hasForm && _.isPlainObject(values)) {
+        formContainer.getForm(name)?.setValues(values)
+      }
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [values],
   )
+
+  useEffect(() => () => formContainer.releaseForm(name), [name])
 
   return useMemo<Form>(() => formContainer.getForm(name) as Form, [name])
 }
