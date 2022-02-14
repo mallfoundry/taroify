b1554e663621cad52ee637e512b73be59cc1bda9
Tang Zhi <tgioer@163.com>
2022-02-14 20:41:40 +0800

    Reference taro types

--- typings.d.ts
+++ typings.d.ts
@@ -1,13 +1,16 @@
-declare module "*.png";
-declare module "*.gif";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.svg";
-declare module "*.css";
-declare module "*.less";
-declare module "*.scss";
-declare module "*.sass";
-declare module "*.styl";
+/// <reference path="node_modules/@tarojs/plugin-platform-weapp/types/shims-weapp.d.ts" />
+/// <reference path="node_modules/@tarojs/taro/types/index.d.ts" />
+
+declare module "*.png"
+declare module "*.gif"
+declare module "*.jpg"
+declare module "*.jpeg"
+declare module "*.svg"
+declare module "*.css"
+declare module "*.less"
+declare module "*.scss"
+declare module "*.sass"
+declare module "*.styl"
 
 declare namespace NodeJS {
   interface ProcessEnv {
