ad6571598702307d9af598b3b6925d35d0a14aa5
沈宗周 <642997741@qq.com>
2022-02-16 16:08:39 +0800

    Update timeline components templates and styles

--- packages/core/src/timeline/timeline-item.tsx
+++ packages/core/src/timeline/timeline-item.tsx
@@ -1,13 +1,29 @@
-import { View } from "@tarojs/components"
+import { View ,Text} from "@tarojs/components"
 import { ViewProps } from "@tarojs/components/types/View"
 import classNames from "classnames"
 import * as React from "react"
+import { ReactNode } from "react"
 import { prefixClassname } from "../styles"
-
-export interface TimeLineItemProps extends ViewProps {}
+import "./timeline-item.scss"
+export interface TimeLineItemProps extends ViewProps {
+  bullet?: JSX.Element
+  title?: string
+  children?: ReactNode
+}
 
 function TimeLineItem(props: TimeLineItemProps) {
-  return <>TimeLineItem</>
+  const { children, bullet, title } = props
+  return (
+    <View className={classNames(prefixClassname("timeline-item"))}>
+      <View className={classNames(prefixClassname("timeline-item-bullet"))}>{bullet}</View>
+      <View className={classNames(prefixClassname("timeline-item-content"))}>
+        <View className={classNames(prefixClassname("timeline-item-content-title"))}>
+          <Text>{title}</Text>
+        </View>
+        {children}
+      </View>
+    </View>
+  )
 }
 
 export default TimeLineItem
--- packages/core/src/timeline/timeline.tsx
+++ packages/core/src/timeline/timeline.tsx
@@ -3,18 +3,15 @@ import { ViewProps } from "@tarojs/components/types/View"
 import classNames from "classnames"
 import * as React from "react"
 import { ReactNode } from "react"
-
 import { prefixClassname } from "../styles"
-
+import "./timeline.scss"
 export interface TimeLineProps extends ViewProps {
-  children:ReactNode
+  children: ReactNode
 }
 
 function TimeLine(props: TimeLineProps) {
-  const {children} = props
-  return <>TimeLine
-  {children}
-  </>
+  const { children } = props
+  return <View className={classNames(prefixClassname("timeline"))}>{children}</View>
 }
 
 export default TimeLine
