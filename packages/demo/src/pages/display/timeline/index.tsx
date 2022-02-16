c93a4c763daffe09da1783c3f5bd3d5341f182fe
沈宗周 <642997741@qq.com>
2022-02-16 16:09:03 +0800

    Update timeline  explan demo

--- packages/demo/src/pages/display/timeline/index.tsx
+++ packages/demo/src/pages/display/timeline/index.tsx
@@ -1,17 +1,37 @@
 import { Timeline } from "@taroify/core"
-import { Cross, LocationOutlined, SettingOutlined } from "@taroify/icons"
-import * as _ from "lodash"
+import { SettingOutlined } from "@taroify/icons"
+import { View, Text } from "@tarojs/components"
 import * as React from "react"
 import Block from "../../../components/block"
 import Page from "../../../components/page"
 import "./index.scss"
 
-export default function AvatarDemo() {
+export default function TimelineDemo() {
   return (
-    <Page title="Avatar 头像" className="avatar-demo">
+    <Page title="Timeline 时间轴" className="avatar-demo">
       <Block title="基础用法" className="avatar-block">
         <Timeline>
-          <Timeline.Item></Timeline.Item>
+          <Timeline.Item bullet={<SettingOutlined size={12} />} title="New branch">
+            <View>
+              You&apos;ve created new branch{" "}
+              <Text style={{ color: "#1c7ed6" }}>fix-notifications</Text> from master
+            </View>
+            <View>2 hours ago</View>
+          </Timeline.Item>
+          <Timeline.Item bullet={<SettingOutlined size={12} />} title="New branch">
+            <View>
+              You&apos;ve created new branch{" "}
+              <Text style={{ color: "#1c7ed6" }}>fix-notifications</Text> from master
+            </View>
+            <View>2 hours ago</View>
+          </Timeline.Item>
+          <Timeline.Item bullet={<SettingOutlined size={12} />} title="New branch">
+            <View>
+              You&apos;ve created new branch{" "}
+              <Text style={{ color: "#1c7ed6" }}>fix-notifications</Text> from master
+            </View>
+            <View>2 hours ago</View>
+          </Timeline.Item>
         </Timeline>
       </Block>
     </Page>
