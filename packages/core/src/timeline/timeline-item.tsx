06313d0f60af3ed1178f57a75db713816bd72b35
沈宗周 <642997741@qq.com>
2022-02-17 13:12:17 +0800

    Updata Timeline algin  attribute

--- packages/core/src/timeline/timeline-item.tsx
+++ packages/core/src/timeline/timeline-item.tsx
@@ -1,24 +1,33 @@
-import { View ,Text} from "@tarojs/components"
+import { View, Text } from "@tarojs/components"
 import { ViewProps } from "@tarojs/components/types/View"
 import classNames from "classnames"
 import * as React from "react"
 import { ReactNode } from "react"
 import { prefixClassname } from "../styles"
+import { algin } from "./timeline.shared"
 import "./timeline-item.scss"
 export interface TimeLineItemProps extends ViewProps {
+  algin?: algin
   bullet?: JSX.Element
   title?: string
   children?: ReactNode
+  className?: string
 }
 
 function TimeLineItem(props: TimeLineItemProps) {
-  const { children, bullet, title } = props
+  const { children, bullet, title, algin = "left", className } = props
   return (
-    <View className={classNames(prefixClassname("timeline-item"))}>
+    <View
+      className={classNames(
+        prefixClassname("timeline-item"),
+        {
+          [prefixClassname("timeline-item-algin")]: algin === "right",
+        },
+        className,
+      )}
+    >
       <View className={classNames(prefixClassname("timeline-item-line"))}>
-        <View className={classNames(prefixClassname("timeline-item-line-bullet"))}>
-        {bullet}
-        </View>
+        <View className={classNames(prefixClassname("timeline-item-line-bullet"))}>{bullet}</View>
       </View>
       <View className={classNames(prefixClassname("timeline-item-content"))}>
         <View className={classNames(prefixClassname("timeline-item-content-title"))}>
--- packages/core/src/timeline/timeline.tsx
+++ packages/core/src/timeline/timeline.tsx
@@ -2,16 +2,30 @@ import { View } from "@tarojs/components"
 import { ViewProps } from "@tarojs/components/types/View"
 import classNames from "classnames"
 import * as React from "react"
-import { ReactNode } from "react"
+import { ReactNode, Children, cloneElement } from "react"
 import { prefixClassname } from "../styles"
+import { algin } from "./timeline.shared"
 import "./timeline.scss"
 export interface TimeLineProps extends ViewProps {
   children: ReactNode
+  algin?: algin
 }
-
+function useTimeLineItemComponents(children: ReactNode, algin: algin | undefined) {
+  if (algin) {
+    return Children.toArray(children).map((child: any) => {
+      return cloneElement(child, {
+        algin: algin ? algin : undefined,
+        className: algin && classNames(prefixClassname("timeline-item-flex")),
+      })
+    })
+  }else{
+    return children
+  }
+}
 function TimeLine(props: TimeLineProps) {
-  const { children } = props
-  return <View className={classNames(prefixClassname("timeline"))}>{children}</View>
+  const { children, algin } = props
+  const TimelineItem = useTimeLineItemComponents(children, algin)
+  return <View className={classNames(prefixClassname("timeline"))}>{TimelineItem}</View>
 }
 
 export default TimeLine
