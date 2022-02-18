0145c0ba012532dd1cbd841c844916d6b59155df
Tang Zhi <tgioer@163.com>
2022-02-18 15:44:57 +0800

    Fix repeated updates with the same value

--- packages/hooks/src/use-uncontrolled/use-uncontrolled.ts
+++ packages/hooks/src/use-uncontrolled/use-uncontrolled.ts
@@ -23,11 +23,16 @@ export default function useUncontrolled<S>(options: UseUncontrolledOptions<S> = 
 
   const setValue = useCallback(
     (newValue: S, emitChange?: (aValue: S) => void) => {
-      if (_.isUndefined(valueRef.current)) {
-        stateRef.current = newValue
-        forceUpdate()
+      // When state was changed,
+      // does set value and trigger onChange event
+      const changed = stateRef.current !== newValue
+      if (changed) {
+        if (_.isUndefined(valueRef.current)) {
+          stateRef.current = newValue
+          forceUpdate()
+        }
+        ;(emitChange ?? onChange)?.(newValue)
       }
-      ;(emitChange ?? onChange)?.(newValue)
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [onChange],
