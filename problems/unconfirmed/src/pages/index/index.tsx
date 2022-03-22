7832aeae6fd906fcb21135d10c98ff776324ceb9
Tang Zhi <tgioer@163.com>
2022-03-22 15:03:21 +0800

    Update test code

--- problems/unconfirmed/src/pages/index/index.tsx
+++ problems/unconfirmed/src/pages/index/index.tsx
@@ -1,20 +1,9 @@
-import { FixedView, Tabbar } from "@taroify/core"
-import { FriendsOutlined, HomeOutlined, Search, SettingOutlined } from "@taroify/icons"
+import { View } from "@tarojs/components"
 import * as React from "react"
 import "./index.scss"
 
 function Index() {
-  return (
-    <>
-      <FixedView placeholder>Test</FixedView>
-      <Tabbar fixed placeholder>
-        <Tabbar.TabItem icon={<HomeOutlined />}>标签</Tabbar.TabItem>
-        <Tabbar.TabItem icon={<Search />}>标签</Tabbar.TabItem>
-        <Tabbar.TabItem icon={<FriendsOutlined />}>标签</Tabbar.TabItem>
-        <Tabbar.TabItem icon={<SettingOutlined />}>标签</Tabbar.TabItem>
-      </Tabbar>
-    </>
-  )
+  return <View>Test</View>
 }
 
 export default Index
