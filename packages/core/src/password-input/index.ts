0a51fae382ec382cfe94a7fcc6fa1b5e17aa24fe
Tang Zhi <tgioer@163.com>
2022-04-04 17:58:04 +0800

    Optimize the PasswordInput component

--- packages/core/src/password-input/index.ts
+++ packages/core/src/password-input/index.ts
@@ -1,1 +1,15 @@
-export { default } from "./password-input"
+import { FunctionComponent } from "react"
+import PasswordInputComponent, { PasswordInputProps } from "./password-input"
+import PasswordInputFeedback from "./password-input-feedback"
+
+interface PasswordInputInterface extends FunctionComponent<PasswordInputProps> {
+  (props: PasswordInputProps): JSX.Element
+
+  Feedback: typeof PasswordInputFeedback
+}
+
+const PasswordInput = PasswordInputComponent as PasswordInputInterface
+
+PasswordInput.Feedback = PasswordInputFeedback
+
+export default PasswordInput
