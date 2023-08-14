88e0ed81314d8351f47481423262b6741b039118
450669540 <36914744+450669540@users.noreply.github.com>
2023-08-14 10:03:15 +0800

    fix：WeChat mini program Click Event detail

--- packages/core/src/utils/dom/event.ts
+++ packages/core/src/utils/dom/event.ts
@@ -27,7 +27,7 @@ interface ClientCoordinates {
 
 export function getClientCoordinates(event: ITouchEvent | MouseEvent): ClientCoordinates {
   // @ts-ignore
-  const { clientX, clientY, detail } = event
+  const { clientX, clientY, detail = {}, touchs = [] } = event
 
   if (clientX && clientY) {
     return {
@@ -35,5 +35,5 @@ export function getClientCoordinates(event: ITouchEvent | MouseEvent): ClientCoo
       clientY,
     }
   }
-  return detail;
+  return touchs[0] || { clientX: detail.x || detail.clientX, clientY: detail.y || detail.clientY };
 }
