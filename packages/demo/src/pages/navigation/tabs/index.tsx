7d3d19c6b5daa0ce4f7af3846f97ddbb693e00b4
Pilotager <yin.bighang@gmail.com>
2024-01-17 19:07:03 +0800

    feat: Add dot and bag attributes to the Tab tab

--- packages/demo/src/pages/navigation/tabs/index.tsx
+++ packages/demo/src/pages/navigation/tabs/index.tsx
@@ -1,5 +1,5 @@
 import { Tabs, Toast } from "@taroify/core"
-import { MoreOutlined } from "@taroify/icons"
+import { MoreOutlined, Success } from "@taroify/icons"
 import Block from "../../../components/block"
 import CustomWrapper from "../../../components/custom-wrapper"
 import Page from "../../../components/page"
@@ -12,6 +12,17 @@ function BasicTabs() {
       <Tabs.TabPane title="标签 2">内容 2</Tabs.TabPane>
       <Tabs.TabPane title="标签 3">内容 3</Tabs.TabPane>
       <Tabs.TabPane title="标签 4">内容 4</Tabs.TabPane>
+    </Tabs>
+  )
+}
+
+function BadgeTabs() {
+  return (
+    <Tabs>
+      <Tabs.TabPane title="标签 1" dot>内容 1</Tabs.TabPane>
+      <Tabs.TabPane title="标签 2" badge={8}>内容 2</Tabs.TabPane>
+      <Tabs.TabPane title="标签 3" badge="Hot">内容 3</Tabs.TabPane>
+      <Tabs.TabPane title="标签 4" badge={<Success className="badge-icon" />}>内容 4</Tabs.TabPane>
     </Tabs>
   )
 }
@@ -157,6 +168,9 @@ export default function TabsDemo() {
     <Page title="Tabs 标签页" className="tabs-demo">
       <Block title="基础用法">
         <BasicTabs />
+      </Block>
+      <Block title="徽标用法">
+        <BadgeTabs />
       </Block>
       <Block title="通过标识匹配">
         <KeyedTabs />
