bafa66b773f395ea6c5bd819f1255be9bde9c75a
Pilotager <yin.bighang@gmail.com>
2024-01-25 19:19:56 +0800

    feat: Doc Search

--- site/src/components/header.tsx
+++ site/src/components/header.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { DocSearch } from "@docsearch/react"
 import { prefixClassname } from "../styles/prefix"
 import github from "./github.svg"
 import "./header.scss"
@@ -12,6 +13,11 @@ export default function Handler() {
           <img src="https://img01.yzcdn.cn/vant/logo.png" alt="taroify logo" />
           <span>Taroify</span>
         </div>
+        <DocSearch
+          appId="R2IYF7ETH7"
+          apiKey="599cec31baffa4868cae4e79f180729b"
+          indexName="docsearch"
+        />
         <ul className={prefixClassname("header__top-nav")}>
           <li className={prefixClassname("header__top-nav-item")}>
             <a href="https://github.com/3lang3/react-vant/">
