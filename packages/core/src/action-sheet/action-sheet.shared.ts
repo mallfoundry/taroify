8cd407cd1938e0aa34f0d853525f2d8d1afa6f18
hbztd <38969912+hbztd@users.noreply.github.com>
2024-06-22 13:36:41 +0800

    feat(ActionSheet): add description, cancelText, action property

--- packages/core/src/action-sheet/action-sheet.shared.ts
+++ packages/core/src/action-sheet/action-sheet.shared.ts
@@ -3,7 +3,12 @@ import Sheet from "../sheet"
 
 export interface ActionSheetActionObject extends Sheet.ItemObject {
   name?: ReactNode
+  subname?: ReactNode
+  disabled?: boolean
+  loading?: boolean
+  className?: string
   value?: any
+  [key: string]: any
 }
 
 export type ActionSheetThemeVars = {
--- packages/demo/src/pages/action/action-sheet/index.tsx
+++ packages/demo/src/pages/action/action-sheet/index.tsx
@@ -1,91 +1,101 @@
 import { ActionSheet, Cell } from "@taroify/core"
 import { ArrowRight } from "@taroify/icons"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 import Block from "../../../components/block"
 import Page from "../../../components/page"
 import "./index.scss"
 
 function BasicActionSheet() {
   const [open, setOpen] = useState(false)
+  const actions = useMemo(() => [
+    { name: "选项一", value: "1" },
+    { name: "选项二", value: "2" },
+    { name: "选项三", value: "3" },
+  ], [])
   return (
     <>
-      <Cell clickable title="基础用法" rightIcon={<ArrowRight />} onClick={() => setOpen(true)} />
-      <ActionSheet open={open} onSelect={() => setOpen(false)} onClose={setOpen}>
-        <ActionSheet.Action value="1" name="选项一" />
-        <ActionSheet.Action value="2" name="选项二" />
-        <ActionSheet.Action value="3" name="选项三" />
-      </ActionSheet>
+      <Cell clickable isLink title="基础用法"  onClick={() => setOpen(true)} />
+      <ActionSheet actions={actions} open={open} onSelect={() => setOpen(false)} onClose={setOpen} />
     </>
   )
 }
 
 function ActionSheetWithCancel() {
   const [open, setOpen] = useState(false)
+  const actions = useMemo(() => [
+    { name: "选项一", value: "1" },
+    { name: "选项二", value: "2" },
+    { name: "选项三", value: "3" },
+  ], [])
   return (
     <>
       <Cell
         clickable
+        isLink
         title="展示取消按钮"
-        rightIcon={<ArrowRight />}
         onClick={() => setOpen(true)}
       />
       <ActionSheet
+        cancelText="取消"
+        actions={actions}
         open={open}
         onSelect={() => setOpen(false)}
         onCancel={() => setOpen(false)}
         onClose={setOpen}
-      >
-        <ActionSheet.Action value="1" name="选项一" />
-        <ActionSheet.Action value="2" name="选项二" />
-        <ActionSheet.Action value="3" name="选项三" />
-        <ActionSheet.Button type="cancel">取消</ActionSheet.Button>
-      </ActionSheet>
+      />
     </>
   )
 }
 
 function ActionSheetWithDescription() {
   const [open, setOpen] = useState(false)
+  const actions = useMemo(() => [
+    { name: "选项一", value: "1", subname: "这是一段描述信息" },
+    { name: "选项二", value: "2" },
+    { name: "选项三", value: "3" },
+  ], [])
   return (
     <>
       <Cell
         clickable
         title="展示描述信息"
-        rightIcon={<ArrowRight />}
+        isLink
         onClick={() => setOpen(true)}
       />
       <ActionSheet
+        description="这是一段描述信息"
+        cancelText="取消"
+        actions={actions}
         open={open}
         onSelect={() => setOpen(false)}
         onCancel={() => setOpen(false)}
         onClose={setOpen}
-      >
-        <ActionSheet.Header>这是一段描述信息</ActionSheet.Header>
-        <ActionSheet.Action value="1" name="选项一" />
-        <ActionSheet.Action value="2" name="选项二" />
-        <ActionSheet.Action value="3" name="选项三" />
-        <ActionSheet.Button type="cancel">取消</ActionSheet.Button>
-      </ActionSheet>
+      />
     </>
   )
 }
 
 function ActionSheetWithStatuses() {
   const [open, setOpen] = useState(false)
+  const actions = useMemo(() => [
+    { name: "选项一", value: "1", style: { color: "#ee0a24" } },
+    { name: "选项二", value: "2", disabled: true },
+    { name: "选项三", value: "3", loading: true },
+  ], [])
   return (
     <>
       <Cell clickable title="选项状态" rightIcon={<ArrowRight />} onClick={() => setOpen(true)} />
       <ActionSheet
+        actions={actions}
+        cancelText="取消"
         open={open}
-        onSelect={() => setOpen(false)}
+        onSelect={(value) => {
+          console.log(value)
+          setOpen(false)
+        }}
         onCancel={() => setOpen(false)}
         onClose={setOpen}
-      >
-        <ActionSheet.Action value="1" style={{ color: "#ee0a24" }} name="着色选项" />
-        <ActionSheet.Action value="2" disabled name="禁止选项" />
-        <ActionSheet.Action value="3" loading name="选项三" />
-        <ActionSheet.Button type="cancel">取消</ActionSheet.Button>
-      </ActionSheet>
+      />
     </>
   )
 }
