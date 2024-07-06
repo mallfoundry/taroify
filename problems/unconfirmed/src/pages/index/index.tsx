a4ac5e3a3c2c7bf5d75d1fae560a3a86c228920b
DESKTOP-ODNE4JH\HuangKuntang <kuntang@163.com>
2024-07-06 08:45:58 +0800

    fix: 修复image组件因为多嵌套一层View引起的样式问题

--- problems/unconfirmed/src/pages/index/index.tsx
+++ problems/unconfirmed/src/pages/index/index.tsx
@@ -1,9 +1,13 @@
 import { View } from "@tarojs/components"
+import { Avatar } from "@taroify/core"
 import * as React from "react"
 import "./index.scss"
 
 function Index() {
-  return <View>Test</View>
+  return <View>
+    <View>测试 Image 组件下影响的 Avatar 情况</View>
+    <Avatar src="https://joesch.moe/api/v1/random"></Avatar>
+  </View>
 }
 
 export default Index
