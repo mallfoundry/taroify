a5ef4f99a299872538a5d9b9bd10e5bcd9890f40
hbztd <38969912+hbztd@users.noreply.github.com>
2024-12-18 15:10:53 +0800

    fix(lodash): replace CallExpression with MemberExpression

--- packages/gulp/__tests__/treeshaking-lodash.test.js
+++ packages/gulp/__tests__/treeshaking-lodash.test.js
@@ -6,12 +6,16 @@ describe("treeShakingLodash", () => {
 import { debounce, throttle, get as mGet } from "lodash";
 import * as _ from "lodash";
 import util from "lodash";
-console.log(mGet(), util.set(), _.map())`)).toEqual(`
+const maxOrMin = true ? _.max : _.min
+console.log(mGet(), util.set(), _.map(), maxOrMin())`)).toEqual(`
 import debounce from "lodash/debounce";
 import throttle from "lodash/throttle";
 import mGet from "lodash/get";
+import _max from "lodash/max";
+import _min from "lodash/min";
 import _map from "lodash/map";
 import utilset from "lodash/set";
-console.log(mGet(), utilset(), _map())`)
+const maxOrMin = true ? _max : _min
+console.log(mGet(), utilset(), _map(), maxOrMin())`)
   })
 })
