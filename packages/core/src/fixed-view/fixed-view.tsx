71899df85e56cdc93e4bca8693db28b60dd4d0d1
Pilotager <yin.bighang@gmail.com>
2025-01-19 17:59:58 +0800

    fix(SafeArea): 替换成 getWindowInfo 方法

--- packages/core/src/fixed-view/fixed-view.tsx
+++ packages/core/src/fixed-view/fixed-view.tsx
@@ -1,37 +1,37 @@
-import { View } from "@tarojs/components"
-import { ViewProps } from "@tarojs/components/types/View"
-import classNames from "classnames"
-import * as _ from "lodash"
-import * as React from "react"
-import { PropsWithChildren, useRef } from "react"
-import { PlaceholderProps, usePlaceholder } from "../placeholder"
-import SafeArea, { SafeAreaPosition } from "../safe-area"
-import { prefixClassname } from "../styles"
-import { FixedViewPosition } from "./fixed-view.shared"
+import { View } from "@tarojs/components";
+import type { ViewProps } from "@tarojs/components/types/View";
+import classNames from "classnames";
+import * as _ from "lodash";
+import * as React from "react";
+import { type PropsWithChildren, useRef } from "react";
+import { type PlaceholderProps, usePlaceholder } from "../placeholder";
+import SafeArea, { type SafeAreaPosition } from "../safe-area";
+import { prefixClassname } from "../styles";
+import type { FixedViewPosition } from "./fixed-view.shared";
 
 function useFixedViewPlaceholder(placeholder?: boolean | string | PlaceholderProps) {
   if (placeholder === true) {
     return {
       className: prefixClassname("fixed-view__placeholder"),
-    }
+    };
   }
 
   if (_.isString(placeholder)) {
     return {
       className: placeholder,
-    }
+    };
   }
 
   if (_.isObject(placeholder)) {
-    return placeholder
+    return placeholder;
   }
 }
 
 interface FixedViewProps extends PropsWithChildren<ViewProps> {
-  position?: boolean | FixedViewPosition
-  safeArea?: SafeAreaPosition
-  nativeSafeTop?: boolean
-  placeholder?: boolean | string | Omit<PlaceholderProps, "children">
+  position?: boolean | FixedViewPosition;
+  safeArea?: SafeAreaPosition;
+  nativeSafeTop?: boolean;
+  placeholder?: boolean | string | Omit<PlaceholderProps, "children">;
 }
 
 function FixedView<T>(props: FixedViewProps & T) {
@@ -43,13 +43,13 @@ function FixedView<T>(props: FixedViewProps & T) {
     placeholder: placeholderProp,
     children,
     ...restProps
-  } = props
-  const rootRef = useRef()
-  const placeholder = useFixedViewPlaceholder(placeholderProp)
-  const Placeholder = usePlaceholder(rootRef)
+  } = props;
+  const rootRef = useRef();
+  const placeholder = useFixedViewPlaceholder(placeholderProp);
+  const Placeholder = usePlaceholder(rootRef);
 
   if (position !== "top" && position !== "bottom" && position !== true) {
-    return children as JSX.Element
+    return children as JSX.Element;
   }
 
   const content = (
@@ -69,20 +69,20 @@ function FixedView<T>(props: FixedViewProps & T) {
       {children}
       {safeArea === "bottom" && <SafeArea position="bottom" />}
     </View>
-  )
+  );
 
   if (placeholder) {
-    const { className: placeholderClassName, ...restPlaceholder } = placeholder
+    const { className: placeholderClassName, ...restPlaceholder } = placeholder;
     return (
       <Placeholder
         className={classNames(prefixClassname("fixed-view__placeholder"), placeholderClassName)}
         {...restPlaceholder}
         children={content}
       />
-    )
+    );
   }
 
-  return content
+  return content;
 }
 
-export default FixedView
+export default FixedView;
