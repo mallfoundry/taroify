0c5b2f6aa3f7bba72e29f5ed1da8b8fcc79a28f8
Pilotager <yin.bighang@gmail.com>
2026-07-21 08:20:07 +0800

    test(ActionSheet): 补全组件测试

--- packages/core/src/action-sheet/index.ts
+++ packages/core/src/action-sheet/index.ts
@@ -1,7 +1,7 @@
-import ShareSheetHeader from "../share-sheet/share-sheet-header"
 import Sheet from "../sheet"
 import ActionSheetComponent, { type ActionSheetProps } from "./action-sheet"
 import ActionSheetAction from "./action-sheet-action"
+import ActionSheetHeader from "./action-sheet-header"
 import type { ActionSheetActionObject } from "./action-sheet.shared"
 
 export type { ActionSheetActionObject, ActionSheetThemeVars } from "./action-sheet.shared"
@@ -10,14 +10,14 @@ interface ActionSheetInterface {
   (props: ActionSheetProps): JSX.Element
 
   Backdrop: typeof Sheet.Backdrop
-  Header: typeof ShareSheetHeader
+  Header: typeof ActionSheetHeader
   Action: typeof ActionSheetAction
   Button: typeof Sheet.Button
 }
 
 const ActionSheet = ActionSheetComponent as ActionSheetInterface
 ActionSheet.Backdrop = Sheet.Backdrop
-ActionSheet.Header = ShareSheetHeader
+ActionSheet.Header = ActionSheetHeader
 ActionSheet.Action = ActionSheetAction
 ActionSheet.Button = Sheet.Button
 
